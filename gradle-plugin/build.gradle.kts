469b909c95d7e0a2384fa65131ef45fe966ba6b7
Victor Turansky <victor.turansky@gmail.com>
2020-07-17 00:56:44 +0300

    Gradle plugin configuration

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -21,6 +21,34 @@ pluginPublish {
     gradlePluginPrefix = true
 }
 
+gradlePlugin {
+    plugins {
+        create("seskar") {
+            id = "com.github.turansky.seskar"
+            implementationClass = "com.github.turansky.seskar.gradle.plugin.SeskarGradleSubplugin"
+        }
+    }
+}
+
+val REPO_URL = "https://github.com/turansky/seskar"
+
+pluginBundle {
+    website = REPO_URL
+    vcsUrl = REPO_URL
+
+    plugins.getByName("seskar") {
+        displayName = "Seskar"
+        description = "Additions to Kotlin data classes"
+        tags = listOf(
+            "kotlin",
+            "dataclass",
+            "equals",
+            "hashcode"
+        )
+        version = project.version.toString()
+    }
+}
+
 // TODO: remove after migration on 1.4
 tasks.compileKotlin {
     kotlinOptions.allWarningsAsErrors = false
