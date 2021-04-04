70767ad68917064c518cf27782a8878fafc3f19b
Victor Turansky <victor.turansky@gmail.com>
2021-04-04 04:14:23 +0300

    Kotlin 1.4.32

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     `java-gradle-plugin`
 
-    id("com.gradle.plugin-publish") version "0.12.0"
-    id("com.github.turansky.kfc.plugin-publish") version "0.11.0"
+    id("com.gradle.plugin-publish") version "0.14.0"
+    id("com.github.turansky.kfc.plugin-publish") version "3.6.1"
 
-    kotlin("jvm") version "1.4.0"
+    kotlin("jvm") version "1.4.32"
 }
 
 repositories {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ rootProject.name = "seskar"
 
 pluginManagement {
     plugins {
-        val kotlinVersion = "1.4.0"
+        val kotlinVersion = "1.4.32"
         kotlin("jvm") version kotlinVersion
         kotlin("js") version kotlinVersion
     }
