47e56da5b75663592aae2b23e34ccbff90c7aa07
Victor Turansky <victor.turansky@gmail.com>
2021-04-11 16:47:02 +0300

    Update test example

---
+++ example/src/main/kotlin/com/test/example/Align.kt
@@ -0,0 +1,10 @@
+package com.test.example
+
+external enum class Align {
+    TOP,
+    LEFT,
+    BOTTOM,
+    RIGHT,
+
+    ;
+}
--- example/src/main/kotlin/com/test/example/User.kt
+++
@@ -1,9 +0,0 @@
-package com.test.example
-
-@JsExport
-@ExperimentalJsExport
-data class User(
-    val firstName: String,
-    val lastName: String,
-    val age: Int,
-)
