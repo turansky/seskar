1316d21c9149b8e45099eb06d0ef358df466fdea
Victor Turansky <victor.turansky@gmail.com>
2021-04-11 17:19:43 +0300

    Add test example

---
+++ example/src/main/kotlin/com/test/example/Main.kt
@@ -0,0 +1,14 @@
+package com.test.example
+
+val T = Align.TOP
+val L = Align.LEFT
+
+fun main() {
+    println("TOP ALIGN")
+    println(Align.TOP)
+    println(T)
+
+    println("LEFT ALIGN")
+    println(Align.LEFT)
+    println(L)
+}
