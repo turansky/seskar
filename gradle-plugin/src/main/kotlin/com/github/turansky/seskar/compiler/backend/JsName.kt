42f24b9e4fced802ac63e249b6fc1a17a0bd1430
Victor Turansky <victor.turansky@gmail.com>
2021-04-12 03:24:13 +0300

    Separate `JsName` util method

---
+++ gradle-plugin/src/main/kotlin/com/github/turansky/seskar/compiler/backend/JsName.kt
@@ -0,0 +1,42 @@
+package com.github.turansky.seskar.compiler.backend
+
+import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
+import org.jetbrains.kotlin.ir.IrElement
+import org.jetbrains.kotlin.ir.expressions.IrConstructorCall
+import org.jetbrains.kotlin.ir.expressions.impl.IrConstImpl
+import org.jetbrains.kotlin.ir.expressions.impl.IrConstructorCallImpl
+import org.jetbrains.kotlin.ir.types.defaultType
+import org.jetbrains.kotlin.name.FqName
+
+private val JS_NAME = FqName("kotlin.js.JsName")
+
+internal fun JsName(
+    context: IrPluginContext,
+    source: IrElement,
+    name: String,
+): IrConstructorCall {
+    val type = context.referenceClass(JS_NAME)!!.defaultType
+    val symbol = context.referenceConstructors(JS_NAME).single()
+    val annotation = IrConstructorCallImpl(
+        startOffset = source.startOffset,
+        endOffset = source.endOffset,
+        type = type,
+        symbol = symbol,
+        typeArgumentsCount = 0,
+        constructorTypeArgumentsCount = 0,
+        valueArgumentsCount = 1,
+        origin = null,
+    )
+
+    val nameParameter = IrConstImpl.string(
+        startOffset = source.startOffset,
+        endOffset = source.endOffset,
+        type = context.irBuiltIns.stringType,
+        value = name,
+    )
+
+    annotation.putValueArgument(0, nameParameter)
+
+    return annotation
+}
+
--- gradle-plugin/src/main/kotlin/com/github/turansky/seskar/compiler/backend/UnionTransformer.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/seskar/compiler/backend/UnionTransformer.kt
@@ -4,42 +4,16 @@ import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.descriptors.ClassKind
 import org.jetbrains.kotlin.ir.IrStatement
 import org.jetbrains.kotlin.ir.declarations.IrClass
-import org.jetbrains.kotlin.ir.expressions.impl.IrConstImpl
-import org.jetbrains.kotlin.ir.expressions.impl.IrConstructorCallImpl
-import org.jetbrains.kotlin.ir.types.defaultType
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
-import org.jetbrains.kotlin.name.FqName
-
-private val JS_NAME = FqName("kotlin.js.JsName")
 
 internal class UnionTransformer(
     private val context: IrPluginContext
 ) : IrElementTransformerVoid() {
     override fun visitClass(declaration: IrClass): IrStatement {
         if (declaration.isExternal && declaration.kind == ClassKind.ENUM_CLASS) {
-            val jsNameType = context.referenceClass(JS_NAME)!!.defaultType
-            val jsName = context.referenceConstructors(JS_NAME).single()
-            val annotation = IrConstructorCallImpl(
-                startOffset = declaration.startOffset,
-                endOffset = declaration.endOffset,
-                type = jsNameType,
-                symbol = jsName,
-                typeArgumentsCount = 0,
-                constructorTypeArgumentsCount = 0,
-                valueArgumentsCount = 1,
-                origin = null,
-            )
+            val name = "SeskarUnion" + declaration.name.identifier
 
-            val name = IrConstImpl.string(
-                startOffset = declaration.startOffset,
-                endOffset = declaration.endOffset,
-                type = context.irBuiltIns.stringType,
-                value = "Seskar" + declaration.name.identifier
-            )
-
-            annotation.putValueArgument(0, name)
-
-            declaration.annotations += annotation
+            declaration.annotations += JsName(context, declaration, name)
         }
 
         return super.visitClass(declaration)
