0fc64cc86cc3687c2ac37141b2de4cd46cc43bb8
Victor Turansky <victor.turansky@gmail.com>
2021-04-12 03:35:51 +0300

    Calculate union body

--- gradle-plugin/src/main/kotlin/com/github/turansky/seskar/compiler/backend/UnionTransformer.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/seskar/compiler/backend/UnionTransformer.kt
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.descriptors.ClassKind
 import org.jetbrains.kotlin.ir.IrStatement
 import org.jetbrains.kotlin.ir.declarations.IrClass
+import org.jetbrains.kotlin.ir.declarations.IrEnumEntry
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
 
 internal class UnionTransformer(
@@ -11,9 +12,13 @@ internal class UnionTransformer(
 ) : IrElementTransformerVoid() {
     override fun visitClass(declaration: IrClass): IrStatement {
         if (declaration.isExternal && declaration.kind == ClassKind.ENUM_CLASS) {
-            val name = "SeskarUnion" + declaration.name.identifier
+            val unionBody = declaration.declarations.asSequence()
+                .filterIsInstance<IrEnumEntry>()
+                .map { it.name.identifier }
+                .map { "$it: '$it'" }
+                .joinToString(",", "{", "}")
 
-            declaration.annotations += JsName(context, declaration, name)
+            declaration.annotations += JsName(context, declaration, unionBody)
         }
 
         return super.visitClass(declaration)
