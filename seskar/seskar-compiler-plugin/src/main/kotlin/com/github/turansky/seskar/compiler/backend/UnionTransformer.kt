3dcff5d83ec593fc6784c16f91230b3a0b945e34
Victor Turansky <victor.turansky@gmail.com>
2021-04-26 05:41:31 +0300

    Fix union check

--- seskar/seskar-compiler-plugin/src/main/kotlin/com/github/turansky/seskar/compiler/backend/UnionTransformer.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/com/github/turansky/seskar/compiler/backend/UnionTransformer.kt
@@ -5,13 +5,20 @@ import org.jetbrains.kotlin.descriptors.ClassKind
 import org.jetbrains.kotlin.ir.IrStatement
 import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrEnumEntry
+import org.jetbrains.kotlin.ir.util.hasAnnotation
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
+import org.jetbrains.kotlin.name.FqName
+
+private val JS_UNION = FqName("seskar.js.JsUnion")
+
+private val IrClass.isJsUnion: Boolean
+    get() = isExternal && kind == ClassKind.ENUM_CLASS && hasAnnotation(JS_UNION)
 
 internal class UnionTransformer(
     private val context: IrPluginContext
 ) : IrElementTransformerVoid() {
     override fun visitClass(declaration: IrClass): IrStatement {
-        if (declaration.isExternal && declaration.kind == ClassKind.ENUM_CLASS) {
+        if (declaration.isJsUnion) {
             val unionBody = declaration.declarations.asSequence()
                 .filterIsInstance<IrEnumEntry>()
                 .map { it.name.identifier }
