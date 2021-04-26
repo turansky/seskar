fbd06905dc0cf8670f18b43fe63bcd4462705019
Victor Turansky <victor.turansky@gmail.com>
2021-04-26 08:31:24 +0300

    Support `@JsValue` annotation

--- seskar/seskar-compiler-plugin/src/main/kotlin/com/github/turansky/seskar/compiler/backend/UnionTransformer.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/com/github/turansky/seskar/compiler/backend/UnionTransformer.kt
@@ -3,16 +3,27 @@ package com.github.turansky.seskar.compiler.backend
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.descriptors.ClassKind
 import org.jetbrains.kotlin.ir.IrStatement
+import org.jetbrains.kotlin.ir.backend.js.utils.getSingleConstStringArgument
 import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrEnumEntry
+import org.jetbrains.kotlin.ir.util.getAnnotation
 import org.jetbrains.kotlin.ir.util.hasAnnotation
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
 import org.jetbrains.kotlin.name.FqName
 
 private val JS_UNION = FqName("seskar.js.JsUnion")
+private val JS_VALUE = FqName("seskar.js.JsValue")
 
 private val IrClass.isJsUnion: Boolean
     get() = isExternal && kind == ClassKind.ENUM_CLASS && hasAnnotation(JS_UNION)
+
+private val IrEnumEntry.id: String
+    get() = name.identifier
+
+private val IrEnumEntry.value: String
+    get() = getAnnotation(JS_VALUE)
+        ?.getSingleConstStringArgument()
+        ?: id
 
 internal class UnionTransformer(
     private val context: IrPluginContext
@@ -21,8 +32,7 @@ internal class UnionTransformer(
         if (declaration.isJsUnion) {
             val unionBody = declaration.declarations.asSequence()
                 .filterIsInstance<IrEnumEntry>()
-                .map { it.name.identifier }
-                .map { "$it: '$it'" }
+                .map { "${it.id}: '${it.value}'" }
                 .joinToString(",", "{", "}")
 
             declaration.annotations += JsName(context, declaration, unionBody)
