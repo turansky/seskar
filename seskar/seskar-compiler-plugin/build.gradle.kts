fb561bd1deedc7732a8f08e87260a5c7a5b40684
Victor Turansky <victor.turansky@gmail.com>
2021-04-26 04:10:24 +0300

    Add publish workaround

--- seskar/seskar-compiler-plugin/build.gradle.kts
+++ seskar/seskar-compiler-plugin/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     kotlin("jvm")
-    /*
-    id("com.github.turansky.kfc.maven-central-publish")
-    */
+    id("com.github.turansky.kfc.maven-central-publish") apply false
 }
+
+// TODO: fix configuration
+apply(plugin = "com.github.turansky.kfc.maven-central-publish")
 
 dependencies {
     compileOnly(kotlin("compiler-embeddable"))
