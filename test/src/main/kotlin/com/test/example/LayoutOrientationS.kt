5c1d69631e76657c0628e56e76f07cc97af8c12a
Victor Turansky <victor.turansky@gmail.com>
2021-05-09 15:06:14 +0300

    Add `LayoutOrientation` enums

---
+++ test/src/main/kotlin/com/test/example/LayoutOrientationS.kt
@@ -0,0 +1,14 @@
+package com.test.example
+
+import seskar.js.Case
+import seskar.js.JsUnion
+
+@JsUnion(case = Case.SNAKE)
+external enum class LayoutOrientationS {
+    TOP_TO_BOTTOM,
+    LEFT_TO_RIGHT,
+    bottomToTop,
+    rightToLeft,
+
+    ;
+}
