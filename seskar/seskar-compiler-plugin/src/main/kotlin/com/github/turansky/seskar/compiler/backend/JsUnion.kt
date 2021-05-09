f2e021f283b3881501ca940d802c511ab3b8c24a
Victor Turansky <victor.turansky@gmail.com>
2021-05-09 20:37:46 +0300

    Move union body calculation to separate file

---
+++ seskar/seskar-compiler-plugin/src/main/kotlin/com/github/turansky/seskar/compiler/backend/JsUnion.kt
@@ -0,0 +1,58 @@
+package com.github.turansky.seskar.compiler.backend
+
+import org.jetbrains.kotlin.descriptors.ClassKind
+import org.jetbrains.kotlin.ir.declarations.IrClass
+import org.jetbrains.kotlin.ir.declarations.IrEnumEntry
+import org.jetbrains.kotlin.ir.expressions.IrConst
+import org.jetbrains.kotlin.ir.expressions.IrConstructorCall
+import org.jetbrains.kotlin.ir.util.getAnnotation
+import org.jetbrains.kotlin.ir.util.hasAnnotation
+import org.jetbrains.kotlin.name.FqName
+
+private val JS_UNION = FqName("seskar.js.JsUnion")
+
+private val JS_INT = FqName("seskar.js.JsInt")
+private val JS_STRING = FqName("seskar.js.JsString")
+
+private fun jsValue(i: Int): String = "$i"
+
+private fun jsValue(s: String): String = "'$s'"
+
+private val IrClass.isJsUnion: Boolean
+    get() = isExternal && kind == ClassKind.ENUM_CLASS && hasAnnotation(JS_UNION)
+
+private val IrEnumEntry.id: String
+    get() = name.identifier
+
+private inline fun <reified T> IrConstructorCall.value(): T {
+    val argument = getValueArgument(0) as IrConst<*>
+    return argument.value as T
+}
+
+private val IrEnumEntry.value: String
+    get() {
+        val jsInt = getAnnotation(JS_INT)
+        if (jsInt != null) {
+            return jsValue(jsInt.value<Int>())
+        }
+
+        val jsString = getAnnotation(JS_STRING)
+        if (jsString != null) {
+            return jsValue(jsString.value<String>())
+        }
+
+        return jsValue(id)
+    }
+
+internal fun IrClass.toJsUnionBody(): String? {
+    if (!isExternal) return null
+    if (kind != ClassKind.ENUM_CLASS) return null
+
+    getAnnotation(JS_UNION) ?: return null
+
+    return declarations.asSequence()
+        .filterIsInstance<IrEnumEntry>()
+        .map { "${it.id}: ${it.value}" }
+        .joinToString(",", "{", "}")
+}
+
--- seskar/seskar-compiler-plugin/src/main/kotlin/com/github/turansky/seskar/compiler/backend/UnionTransformer.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/com/github/turansky/seskar/compiler/backend/UnionTransformer.kt
@@ -1,62 +1,16 @@
 package com.github.turansky.seskar.compiler.backend
 
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
-import org.jetbrains.kotlin.descriptors.ClassKind
 import org.jetbrains.kotlin.ir.IrStatement
 import org.jetbrains.kotlin.ir.declarations.IrClass
-import org.jetbrains.kotlin.ir.declarations.IrEnumEntry
-import org.jetbrains.kotlin.ir.expressions.IrConst
-import org.jetbrains.kotlin.ir.expressions.IrConstructorCall
-import org.jetbrains.kotlin.ir.util.getAnnotation
-import org.jetbrains.kotlin.ir.util.hasAnnotation
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
-import org.jetbrains.kotlin.name.FqName
-
-private val JS_UNION = FqName("seskar.js.JsUnion")
-
-private val JS_INT = FqName("seskar.js.JsInt")
-private val JS_STRING = FqName("seskar.js.JsString")
-
-private fun jsValue(i: Int): String = "$i"
-
-private fun jsValue(s: String): String = "'$s'"
-
-private val IrClass.isJsUnion: Boolean
-    get() = isExternal && kind == ClassKind.ENUM_CLASS && hasAnnotation(JS_UNION)
-
-private val IrEnumEntry.id: String
-    get() = name.identifier
-
-private inline fun <reified T> IrConstructorCall.value(): T {
-    val argument = getValueArgument(0) as IrConst<*>
-    return argument.value as T
-}
-
-private val IrEnumEntry.value: String
-    get() {
-        val jsInt = getAnnotation(JS_INT)
-        if (jsInt != null) {
-            return jsValue(jsInt.value<Int>())
-        }
-
-        val jsString = getAnnotation(JS_STRING)
-        if (jsString != null) {
-            return jsValue(jsString.value<String>())
-        }
-
-        return jsValue(id)
-    }
 
 internal class UnionTransformer(
-    private val context: IrPluginContext
+    private val context: IrPluginContext,
 ) : IrElementTransformerVoid() {
     override fun visitClass(declaration: IrClass): IrStatement {
-        if (declaration.isJsUnion) {
-            val unionBody = declaration.declarations.asSequence()
-                .filterIsInstance<IrEnumEntry>()
-                .map { "${it.id}: ${it.value}" }
-                .joinToString(",", "{", "}")
-
+        val unionBody = declaration.toJsUnionBody()
+        if (unionBody != null) {
             declaration.annotations += JsName(context, declaration, unionBody)
         }
 
