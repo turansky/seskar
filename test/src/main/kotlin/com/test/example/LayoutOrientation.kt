f3621fe9acc9f1154ad4ce8e34e4eedb35d007ff
Victor Turansky <victor.turansky@gmail.com>
2021-05-09 15:27:03 +0300

    Single `LayoutOrientation` file

--- test/src/main/kotlin/com/test/example/LayoutOrientation.kt
+++ test/src/main/kotlin/com/test/example/LayoutOrientation.kt
@@ -3,6 +3,16 @@ package com.test.example
 import seskar.js.Case
 import seskar.js.JsUnion
 
+@JsUnion(case = Case.KEBAB)
+external enum class LayoutOrientationK {
+    TOP_TO_BOTTOM,
+    LEFT_TO_RIGHT,
+    bottomToTop,
+    rightToLeft,
+
+    ;
+}
+
 @JsUnion(case = Case.SNAKE)
 external enum class LayoutOrientationS {
     TOP_TO_BOTTOM,
