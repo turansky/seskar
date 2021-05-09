77805ea8dd1e08aaa3e2e39f19622e38ff03127e
Victor Turansky <victor.turansky@gmail.com>
2021-05-09 15:51:51 +0300

    Add `String` extensions

---
+++ seskar/seskar-compiler-plugin/src/main/kotlin/com/github/turansky/seskar/compiler/backend/Strings.kt
@@ -0,0 +1,10 @@
+package com.github.turansky.seskar.compiler.backend
+
+private val LOWER_TO_UPPER = Regex("([a-z])([A-Z])")
+
+internal fun String.kebab(): String =
+    snake().replace("_", "-")
+
+internal fun String.snake(): String =
+    replace(LOWER_TO_UPPER, "$1_$2")
+        .lowercase()
