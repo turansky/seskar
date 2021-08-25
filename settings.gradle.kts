a42311368d6068af2d19621826e324eecc09e461
Victor Turansky <victor.turansky@gmail.com>
2021-08-25 20:01:45 +0300

    KFC `4.30.0`

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ pluginManagement {
         val kotlinVersion = "1.5.30"
         kotlin("js") version kotlinVersion
 
-        val kfcVersion = "4.12.0"
+        val kfcVersion = "4.30.0"
         id("com.github.turansky.kfc.application") version kfcVersion
     }
 }
