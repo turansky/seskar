bb4e4ebbfe2d41a867c307b56ee39a2a35b821b9
Victor Turansky <victor.turansky@gmail.com>
2022-04-17 05:43:40 +0400

    KFC `5.9.1`

--- isolated-test/build.gradle.kts
+++ isolated-test/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("js") version "1.6.20"
-    id("io.github.turansky.kfc.application") version "5.9.0"
+    id("io.github.turansky.kfc.application") version "5.9.1"
     id("io.github.turansky.seskar") version "0.5.0"
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ pluginManagement {
         val kotlinVersion = "1.6.20"
         kotlin("js") version kotlinVersion
 
-        val kfcVersion = "5.9.0"
+        val kfcVersion = "5.9.1"
         id("io.github.turansky.kfc.application") version kfcVersion
     }
 }
