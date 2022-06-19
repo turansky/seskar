e80309f032e6ff043575fd837b7d351087f4e129
Victor Turansky <victor.turansky@gmail.com>
2022-06-20 02:09:54 +0300

    Empty `ValueTransformer`

---
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/value/backend/ValueTransformer.kt
@@ -0,0 +1,10 @@
+package seskar.compiler.value.backend
+
+import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
+import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
+
+internal class ValueTransformer(
+    private val context: IrPluginContext,
+) : IrElementTransformerVoid() {
+    // TODO: implement
+}
---
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/value/extensions/ValueRegistrar.kt
@@ -0,0 +1,15 @@
+package seskar.compiler.value.extensions
+
+import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
+import org.jetbrains.kotlin.com.intellij.mock.MockProject
+import org.jetbrains.kotlin.compiler.plugin.ComponentRegistrar
+import org.jetbrains.kotlin.config.CompilerConfiguration
+
+class ValueRegistrar : ComponentRegistrar {
+    override fun registerProjectComponents(
+        project: MockProject,
+        configuration: CompilerConfiguration,
+    ) {
+        IrGenerationExtension.registerExtension(project, ValueTransformExtension())
+    }
+}
