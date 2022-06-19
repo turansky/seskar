7066fa1db46ae87169ecdd293d5c01942880968d
Victor Turansky <victor.turansky@gmail.com>
2022-06-19 16:10:27 +0300

    Add test `Props`

---
+++ tests/react/src/main/kotlin/com/test/example/Rect.kt
@@ -0,0 +1,6 @@
+package com.test.example
+
+external interface RectProps : ShapeProps {
+    var width: Double
+    var height: Double
+}
---
+++ tests/react/src/main/kotlin/com/test/example/Shape.kt
@@ -0,0 +1,5 @@
+package com.test.example
+
+import react.Props
+
+external interface ShapeProps : Props
