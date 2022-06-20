77fccfc77ddf25718587a87e9a63940ac7c4f970
Victor Turansky <victor.turansky@gmail.com>
2022-06-20 04:37:04 +0300

    Use `ValueTransformer`

--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/value/backend/ValueTransformer.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/value/backend/ValueTransformer.kt
@@ -1,10 +1,17 @@
 package seskar.compiler.value.backend
 
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
+import org.jetbrains.kotlin.ir.expressions.IrExpression
+import org.jetbrains.kotlin.ir.expressions.IrVararg
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
 
 internal class ValueTransformer(
     private val context: IrPluginContext,
 ) : IrElementTransformerVoid() {
-    // TODO: implement
+    override fun visitVararg(expression: IrVararg): IrExpression {
+        println("VARARG!!!")
+        println(expression.elements.size)
+
+        return super.visitVararg(expression)
+    }
 }
