79c15831ddd04b311d927fe051cbd455eec52c42
Victor Turansky <victor.turansky@gmail.com>
2022-06-20 04:20:24 +0300

    Simple value classes test case

---
+++ tests/value/src/main/kotlin/com/test/example/App.kt
@@ -0,0 +1,19 @@
+package com.test.example
+
+fun main() {
+    val age0 = Age(42)
+    val age1: Any = age0
+    val age2: dynamic = age0
+
+    println(age0)
+    println(age1)
+    println(age2)
+
+    val city0 = City("Amsterdam")
+    val city1: Any = city0
+    val city2: dynamic = city0
+
+    println(city0)
+    println(city1)
+    println(city2)
+}
