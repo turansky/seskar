e7f60ec8aa773d41119f4fa8e524f8abdd9dc47d
Victor Turansky <victor.turansky@gmail.com>
2022-06-20 04:11:12 +0300

    Simple value classes

---
+++ tests/value/src/main/kotlin/com/test/example/Age.kt
@@ -0,0 +1,8 @@
+package com.test.example
+
+import seskar.js.JsValue
+
+@JsValue
+value class Age(
+    val value: Int,
+)
---
+++ tests/value/src/main/kotlin/com/test/example/City.kt
@@ -0,0 +1,8 @@
+package com.test.example
+
+import seskar.js.JsValue
+
+@JsValue
+value class City(
+    val name: String,
+)
