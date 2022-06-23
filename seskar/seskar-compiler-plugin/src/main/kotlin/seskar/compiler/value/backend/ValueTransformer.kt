ee8aaf41ce3849ae5bc7025393701d3361e405d2
Victor Turansky <victor.turansky@gmail.com>
2022-06-23 17:30:44 +0300

    Update value transform logic

--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/value/backend/ValueTransformer.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/value/backend/ValueTransformer.kt
@@ -3,18 +3,35 @@ package seskar.compiler.value.backend
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.ir.expressions.IrExpression
 import org.jetbrains.kotlin.ir.expressions.IrVararg
+import org.jetbrains.kotlin.ir.expressions.IrVarargElement
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
 
 internal class ValueTransformer(
     private val context: IrPluginContext,
 ) : IrElementTransformerVoid() {
-    override fun visitVararg(expression: IrVararg): IrExpression {
-        expression.elements
-            .asSequence()
-            .filterIsInstance<IrExpression>()
-            .filter { downCastRequired(it.type) }
-            .forEach { it.type = context.irBuiltIns.anyNType }
+    override fun visitVararg(
+        expression: IrVararg,
+    ): IrExpression {
+        if (expression.varargElementType == context.irBuiltIns.anyNType) {
+            visitVarargElements(expression.elements)
+        }
 
         return super.visitVararg(expression)
+    }
+
+    private fun visitVarargElements(
+        elements: MutableList<IrVarargElement>,
+    ) {
+        val valueElements = elements.asSequence()
+            .filterIsInstance<IrExpression>()
+            .filter { downCastRequired(it.type) }
+            .toList()
+
+        if (valueElements.isEmpty())
+            return
+
+        for (element in valueElements) {
+            element.type = context.irBuiltIns.anyNType
+        }
     }
 }
