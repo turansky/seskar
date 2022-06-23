fa9269f7ab39680ccb0e72a2c37c0667c2f20f0c
Victor Turansky <victor.turansky@gmail.com>
2022-06-23 19:09:19 +0300

    Update transformation test

--- tests/value/src/test/kotlin/com/test/example/ValueTransformTest.kt
+++ tests/value/src/test/kotlin/com/test/example/ValueTransformTest.kt
@@ -1,22 +1,21 @@
 package com.test.example
 
 import kotlin.test.Test
-import kotlin.test.assertIs
+import kotlin.test.assertEquals
 
-class TypeTest {
+class ValueTransformTest {
     @Test
     fun age_second() {
         val array = getArray("first", Age(42))
-        assertIs<String>(array[0])
-        assertIs<Int>(array[1])
+        assertEquals("first", array[0])
+        assertEquals(42, array[1])
     }
 
     @Test
     fun age_first() {
-        val array = getArray(Age(42), "first")
-        assertIs<Int>(array[0])
-        assertIs<String>(array[1])
-
+        val array = getArray(Age(13), "second")
+        assertEquals(13, array[0])
+        assertEquals("second", array[1])
     }
 }
 
