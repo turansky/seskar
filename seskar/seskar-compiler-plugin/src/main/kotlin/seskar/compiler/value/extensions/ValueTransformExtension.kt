f1687f3ea2e579aba7ec06dfeadb9bfd0aea9e53
Victor Turansky <victor.turansky@gmail.com>
2022-06-24 03:39:17 +0300

    Value transform for JS platform only

--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/value/extensions/ValueTransformExtension.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/value/extensions/ValueTransformExtension.kt
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.ir.visitors.transformChildrenVoid
+import org.jetbrains.kotlin.platform.js.isJs
 import seskar.compiler.value.backend.ValueTransformer
 
 internal class ValueTransformExtension : IrGenerationExtension {
@@ -11,6 +12,10 @@ internal class ValueTransformExtension : IrGenerationExtension {
         moduleFragment: IrModuleFragment,
         pluginContext: IrPluginContext,
     ) {
+        pluginContext.platform
+            ?.takeIf { it.isJs() }
+            ?: return
+
         moduleFragment.transformChildrenVoid(ValueTransformer(pluginContext))
     }
 }
