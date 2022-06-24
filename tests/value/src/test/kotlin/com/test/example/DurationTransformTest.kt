c6c5053b2bbcb658fe06ba51afbcf61957999d9a
Victor Turansky <victor.turansky@gmail.com>
2022-06-24 04:03:08 +0300

    Add `Duration` transformation test

---
+++ tests/value/src/test/kotlin/com/test/example/DurationTransformTest.kt
@@ -0,0 +1,22 @@
+package com.test.example
+
+import kotlin.test.Ignore
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
+
+@Ignore
+class DurationTransformTest {
+    @Test
+    fun seconds_13() {
+        val array = useArray(13.seconds)
+        assertEquals("13s", array[0])
+    }
+
+    @Test
+    fun minutes_42() {
+        val array = useArray(42.minutes)
+        assertEquals("42m", array[0])
+    }
+}
