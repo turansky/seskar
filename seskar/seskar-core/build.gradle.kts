27c91f73a46e7204d4840af3e26e3742cf92acf9
Victor Turansky <victor.turansky@gmail.com>
2022-06-24 14:08:17 +0300

    iOS targets for `seskar-core`

--- seskar/seskar-core/build.gradle.kts
+++ seskar/seskar-core/build.gradle.kts
@@ -2,3 +2,10 @@ plugins {
     id("io.github.turansky.kfc.multiplatform")
     id("io.github.turansky.kfc.maven-central-publish")
 }
+
+kotlin {
+    iosArm32()
+    iosArm64()
+    iosX64()
+    iosSimulatorArm64()
+}
