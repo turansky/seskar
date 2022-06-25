60d4b77dfc69f990388475ebebac4ceb5c139236
Victor Turansky <victor.turansky@gmail.com>
2022-06-25 20:13:12 +0300

    More `Duration` tests

--- tests/value/src/test/kotlin/com/test/example/DurationTransformTest.kt
+++ tests/value/src/test/kotlin/com/test/example/DurationTransformTest.kt
@@ -1,7 +1,9 @@
 package com.test.example
 
+import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.time.Duration
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
@@ -21,4 +23,35 @@ class DurationTransformTest {
         val array = useArray(duration)
         assertEquals("42m", array[0])
     }
+
+    @Test
+    fun seconds_13_nullable() {
+        val duration = getDuration(13.seconds)
+
+        val array = useArray(duration)
+        assertEquals("13s", array[0])
+    }
+
+    @Test
+    fun minutes_42_nullable() {
+        val duration = getDuration(42.minutes)
+
+        val array = useArray(duration)
+        assertEquals("42m", array[0])
+    }
+
+    @Ignore
+    @Test
+    fun null_value() {
+        val duration: Duration? = getDuration(5.seconds, true)
+
+        val array = useArray(duration)
+        assertEquals("null", array[0])
+    }
 }
+
+private fun getDuration(
+    value: Duration,
+    ignore: Boolean = false,
+): Duration? =
+    if (!ignore) value else null
