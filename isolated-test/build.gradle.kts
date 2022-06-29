815ebca28ae2babca072b9095143368a7568cbcd
Victor Turansky <victor.turansky@gmail.com>
2022-06-29 17:09:09 +0300

    KFC `5.50.0`

--- isolated-test/build.gradle.kts
+++ isolated-test/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("js") version "1.7.0"
-    id("io.github.turansky.kfc.application") version "5.45.2"
+    id("io.github.turansky.kfc.application") version "5.50.0"
     id("io.github.turansky.seskar") version "0.13.0"
 }
 
