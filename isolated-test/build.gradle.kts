ef9f1b8c240341ddee8d8e894eddb7c260592653
Victor Turansky <victor.turansky@gmail.com>
2022-11-16 09:02:02 +0200

    Kotlin `1.7.21`

--- isolated-test/build.gradle.kts
+++ isolated-test/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("js") version "1.7.20"
-    id("io.github.turansky.kfc.application") version "5.62.0"
+    kotlin("js") version "1.7.21"
+    id("io.github.turansky.kfc.application") version "5.70.0"
     id("io.github.turansky.seskar") version "0.17.0"
 }
 
