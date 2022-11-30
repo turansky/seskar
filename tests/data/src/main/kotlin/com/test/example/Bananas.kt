189b479bdd3471c510e884c42d2ff9e98cbc01cc
Stefan Thaler <stefan@thaler-it.net>
2022-11-30 16:42:19 +0200

    Add tests for imported value / hook

---
+++ tests/data/src/main/kotlin/com/test/example/Bananas.kt
@@ -0,0 +1,8 @@
+package com.test.example
+
+import seskar.js.JsValue
+
+@JsValue
+value class Bananas(
+    val age: Int,
+)
---
+++ tests/data/src/main/kotlin/com/test/example/useImportedArray.kt
@@ -0,0 +1,6 @@
+package com.test.example
+
+fun useImportedArray(
+    vararg dependencies: Any?,
+): Array<out Any?> =
+    dependencies
