97c45b383f1d16ee6bfc5fba51f61c5b7f57e9b0
Victor Turansky <victor.turansky@gmail.com>
2022-11-30 17:39:18 +0200

    Update dependencies

--- isolated-test/build.gradle.kts
+++ isolated-test/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("js") version "1.7.21"
-    id("io.github.turansky.kfc.application") version "5.70.0"
+    id("io.github.turansky.kfc.application") version "5.71.0"
     id("io.github.turansky.seskar") version "0.20.0"
 }
 
