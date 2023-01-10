667ac835e2baa321767d7dfdfe19afd2ef0eed5f
Victor Turansky <victor.turansky@gmail.com>
2023-01-10 02:53:24 +0200

    Add `coroutines-test` dependency

--- tests/value/build.gradle.kts
+++ tests/value/build.gradle.kts
@@ -3,8 +3,12 @@ plugins {
     id("io.github.turansky.seskar")
 }
 
+val coroutinesVersion = project.property("kotlinx-coroutines.version") as String
+
 dependencies {
     implementation(project(":tests:data"))
     implementation("io.github.turansky.seskar:seskar-core")
+
     testImplementation(kotlin("test-js"))
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
 }
