3df508cd3f2fc7045f9fdfcc562d1b76dd7ba794
Victor Turansky <victor.turansky@gmail.com>
2023-01-10 03:11:25 +0200

    Add `react` dependency

--- tests/value/build.gradle.kts
+++ tests/value/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("io.github.turansky.kfc.application")
+    id("io.github.turansky.kfc.wrappers")
     id("io.github.turansky.seskar")
 }
 
@@ -8,6 +9,7 @@ val coroutinesVersion = project.property("kotlinx-coroutines.version") as String
 dependencies {
     implementation(project(":tests:data"))
     implementation("io.github.turansky.seskar:seskar-core")
+    implementation(wrappers("react"))
 
     testImplementation(kotlin("test-js"))
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
