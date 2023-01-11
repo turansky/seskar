7d1235b36ab1349992318bceea1d1402708f089d
Victor Turansky <victor.turansky@gmail.com>
2023-01-11 12:05:55 +0200

    Add `Counter` test component

---
+++ tests/value/src/main/kotlin/com/test/example/Counter.kt
@@ -0,0 +1,14 @@
+package com.test.example
+
+import react.VFC
+import react.dom.html.ReactHTML.button
+
+val Counter = VFC {
+    button {
+        title = "First button"
+    }
+
+    button {
+        title = "Second button"
+    }
+}
