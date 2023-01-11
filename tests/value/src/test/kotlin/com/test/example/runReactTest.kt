e6427ff32d1eb7b5a17717722d378a640be0716b
Victor Turansky <victor.turansky@gmail.com>
2023-01-11 16:27:55 +0200

    Common `runReactTest`

--- tests/value/src/test/kotlin/com/test/example/runReactTest.kt
+++ tests/value/src/test/kotlin/com/test/example/runReactTest.kt
@@ -10,13 +10,21 @@ import web.html.HTML.div
 import web.html.HTMLElement
 
 fun runReactTest(
-    component: VFC,
     testBody: suspend (container: HTMLElement) -> Unit,
 ) {
     runTest {
         val container = document.createElement(div)
         document.body.appendChild(container)
 
+        testBody(container)
+    }
+}
+
+fun runReactTest(
+    component: VFC,
+    testBody: suspend (container: HTMLElement) -> Unit,
+) {
+    runReactTest { container ->
         var root: Root
 
         act {
