555f0d00aa5ae982f487723150cba704f938623d
Victor Turansky <victor.turansky@gmail.com>
2023-01-11 18:14:57 +0200

    Fix `runReactTest` implementation

--- tests/value/src/test/kotlin/com/test/example/DependenciesTest.kt
+++ tests/value/src/test/kotlin/com/test/example/DependenciesTest.kt
@@ -1,12 +1,13 @@
 package com.test.example
 
+import kotlinx.coroutines.test.TestResult
 import web.html.HTML
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class DependenciesTest {
     @Test
-    fun initial() = runReactTest { container ->
+    fun initial(): TestResult = runReactTest { container ->
         val buttons = container.getElementsByTagName(HTML.button.toString() /* TEMP */)
 
         assertEquals(0, buttons.length, "Buttons count before create")
--- tests/value/src/test/kotlin/com/test/example/runReactTest.kt
+++ tests/value/src/test/kotlin/com/test/example/runReactTest.kt
@@ -1,5 +1,7 @@
 package com.test.example
 
+import kotlinx.coroutines.test.TestResult
+import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
 import react.VFC
 import web.dom.document
@@ -7,20 +9,19 @@ import web.html.HTML.div
 import web.html.HTMLElement
 
 fun runReactTest(
-    testBody: suspend (container: HTMLElement) -> Unit,
-) {
+    testBody: suspend TestScope.(container: HTMLElement) -> Unit,
+): TestResult =
     runTest {
         val container = document.createElement(div)
         document.body.appendChild(container)
 
         testBody(container)
     }
-}
 
 fun runReactTest(
     component: VFC,
-    testBody: suspend (container: HTMLElement) -> Unit,
-) {
+    testBody: suspend TestScope.(container: HTMLElement) -> Unit,
+): TestResult =
     runReactTest { container ->
         val root = createRoot(container, component)
 
@@ -28,4 +29,3 @@ fun runReactTest(
 
         unmount(root)
     }
-}
