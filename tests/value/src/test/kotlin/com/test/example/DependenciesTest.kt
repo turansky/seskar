ae29056aafb2eaaca8f34c53f565224373254ba3
Victor Turansky <victor.turansky@gmail.com>
2023-01-11 16:50:46 +0200

    Use `assertEquals`

--- tests/value/src/test/kotlin/com/test/example/DependenciesTest.kt
+++ tests/value/src/test/kotlin/com/test/example/DependenciesTest.kt
@@ -2,27 +2,21 @@ package com.test.example
 
 import web.html.HTML
 import kotlin.test.Test
-import kotlin.test.expect
+import kotlin.test.assertEquals
 
 class DependenciesTest {
     @Test
     fun initial() = runReactTest { container ->
         val buttons = container.getElementsByTagName(HTML.button.toString() /* TEMP */)
 
-        expect(0, "Buttons count before create") {
-            buttons.length
-        }
+        assertEquals(0, buttons.length, "Buttons count before create")
 
         val root = createRoot(container, Counter)
 
-        expect(2, "Buttons count") {
-            buttons.length
-        }
+        assertEquals(2, buttons.length, "Buttons count")
 
         unmount(root)
 
-        expect(0, "Buttons count after unmount") {
-            buttons.length
-        }
+        assertEquals(0, buttons.length, "Buttons count after unmount")
     }
 }
