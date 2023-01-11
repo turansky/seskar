3c62481b2a9ef6b773b7745068a004ff7c59a41a
Victor Turansky <victor.turansky@gmail.com>
2023-01-11 12:00:30 +0200

    Add `runReactTest` utility method

---
+++ tests/value/src/test/kotlin/com/test/example/runReactTest.kt
@@ -0,0 +1,35 @@
+package com.test.example
+
+import kotlinx.coroutines.test.runTest
+import react.VFC
+import react.create
+import react.dom.client.Root
+import react.dom.client.createRoot
+import react.dom.test.utils.act
+import web.dom.document
+import web.html.HTML.div
+import web.html.HTMLElement
+
+fun runReactTest(
+    component: VFC,
+    testBody: suspend (container: HTMLElement) -> Unit,
+) {
+    runTest {
+        val container = document.createElement(div)
+        document.body.appendChild(container)
+
+        var root: Root
+
+        act {
+            root = createRoot(container)
+            root.render(component.create())
+        }
+
+        testBody(container)
+
+        act {
+            // TODO: fix
+            // root.unmount()
+        }
+    }
+}
