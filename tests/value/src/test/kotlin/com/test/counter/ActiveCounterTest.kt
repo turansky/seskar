a5b4ed36ccdd34677d8ae600753f5143ef3e1994
Victor Turansky <victor.turansky@gmail.com>
2023-01-12 20:43:22 +0200

    Use `Simulate` instead of `dispatchEvent`, which doesn't work

--- tests/value/src/test/kotlin/com/test/counter/ActiveCounterTest.kt
+++ tests/value/src/test/kotlin/com/test/counter/ActiveCounterTest.kt
@@ -3,39 +3,36 @@ package com.test.counter
 import com.test.react.runReactTest
 import js.core.get
 import kotlinx.coroutines.test.TestResult
+import react.dom.test.Simulate
 import react.dom.test.act
-import web.dom.CHANGE
-import web.events.Event
 import web.html.HTML.div
-import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ActiveCounterTest {
     @Test
-    @Ignore
     fun initial(): TestResult = runReactTest(ActiveCounter) { container ->
         val target = container.getElementsByTagName(div)[0]
 
         assertEquals(0, target.dataCount, "Count #0")
 
         act {
-            target.dispatchEvent(Event(Event.CHANGE))
+            Simulate.change(target)
         }
         assertEquals(1, target.dataCount, "Count #1")
 
         act {
-            target.dispatchEvent(Event(Event.CHANGE))
+            Simulate.change(target)
         }
         assertEquals(2, target.dataCount, "Count #2")
 
         act {
-            target.dispatchEvent(Event(Event.CHANGE))
+            Simulate.change(target)
         }
         assertEquals(3, target.dataCount, "Count #3")
 
         act {
-            target.dispatchEvent(Event(Event.CHANGE))
+            Simulate.change(target)
         }
         assertEquals(4, target.dataCount, "Count #4")
     }
--- tests/value/src/test/kotlin/com/test/counter/DependencyTest.kt
+++ tests/value/src/test/kotlin/com/test/counter/DependencyTest.kt
@@ -3,9 +3,8 @@ package com.test.counter
 import com.test.react.runReactTest
 import js.core.get
 import kotlinx.coroutines.test.TestResult
+import react.dom.test.Simulate
 import react.dom.test.act
-import web.dom.CHANGE
-import web.events.Event
 import web.html.HTML.div
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -18,22 +17,22 @@ class DependencyTest {
         assertEquals(0, target.dataCount, "Count #0")
 
         act {
-            target.dispatchEvent(Event(Event.CHANGE))
+            Simulate.change(target)
         }
         assertEquals(0, target.dataCount, "Count #1")
 
         act {
-            target.dispatchEvent(Event(Event.CHANGE))
+            Simulate.change(target)
         }
         assertEquals(0, target.dataCount, "Count #2")
 
         act {
-            target.dispatchEvent(Event(Event.CHANGE))
+            Simulate.change(target)
         }
         assertEquals(0, target.dataCount, "Count #3")
 
         act {
-            target.dispatchEvent(Event(Event.CHANGE))
+            Simulate.change(target)
         }
         assertEquals(0, target.dataCount, "Count #4")
     }
