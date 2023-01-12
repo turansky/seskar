e4c9469729665498c01cf2674f1257338ec1f71d
Victor Turansky <victor.turansky@gmail.com>
2023-01-12 14:36:21 +0200

    Update `Counter` implementation

--- tests/value/src/main/kotlin/com/test/counter/Counter.kt
+++ tests/value/src/main/kotlin/com/test/counter/Counter.kt
@@ -2,13 +2,25 @@ package com.test.counter
 
 import react.VFC
 import react.dom.html.ReactHTML.button
+import react.dom.html.ReactHTML.div
+import react.dom.onChange
+
+const val COUNTER_CONTAINER_ID = "container"
 
 val Counter = VFC {
-    button {
-        title = "First button"
-    }
+    div {
+        id = COUNTER_CONTAINER_ID
 
-    button {
-        title = "Second button"
+        onChange = {
+            // TODO: implement
+        }
+
+        button {
+            title = "First button"
+        }
+
+        button {
+            title = "Second button"
+        }
     }
 }
