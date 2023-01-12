838f2a7122a5b90dac297de0a6a46211d8a44ed1
Victor Turansky <victor.turansky@gmail.com>
2023-01-12 14:55:45 +0200

    Data `count` attribute support

---
+++ tests/value/src/main/kotlin/com/test/counter/dataCount.kt
@@ -0,0 +1,15 @@
+package com.test.counter
+
+import react.Props
+import web.dom.Element
+
+const val DATA_COUNT = "data-count"
+
+var Props.dataCount: Int?
+    get() = asDynamic()[DATA_COUNT]
+    set(value) {
+        asDynamic()[DATA_COUNT] = value
+    }
+
+val Element.dataCount: Int?
+    get() = getAttribute(DATA_COUNT)?.toInt()
