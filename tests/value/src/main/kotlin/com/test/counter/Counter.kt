c472a36d153b867893467618926c47f737cbdf74
Victor Turansky <victor.turansky@gmail.com>
2023-01-12 15:08:22 +0200

    Update counter implementation

--- tests/value/src/main/kotlin/com/test/counter/Counter.kt
+++ tests/value/src/main/kotlin/com/test/counter/Counter.kt
@@ -1,10 +1,13 @@
 package com.test.counter
 
+import com.test.example.Count
 import react.FC
 import react.Props
 import react.dom.html.ReactHTML.button
 import react.dom.html.ReactHTML.div
 import react.dom.onChange
+import react.useEffect
+import react.useState
 
 const val COUNTER_CONTAINER_ID = "container"
 
@@ -12,12 +15,24 @@ external interface CounterProps : Props {
     var active: Boolean
 }
 
-val Counter = FC<CounterProps> {
+val Counter = FC<CounterProps> { props ->
+    val (trigger, setTrigger) = useState(Count(1000))
+
+    val (count, setCount) = useState(0)
+
+    useEffect(trigger) {
+        setCount { it + 1 }
+    }
+
     div {
         id = COUNTER_CONTAINER_ID
+        dataCount = count
 
         onChange = {
-            // TODO: implement
+            val diff = if (props.active) 1 else 0
+            setTrigger {
+                Count(it.value + diff)
+            }
         }
 
         button {
