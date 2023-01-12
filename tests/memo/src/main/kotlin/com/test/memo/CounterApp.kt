ba469cdb7cb82b9139bd08938529199b64c4f4a9
Victor Turansky <victor.turansky@gmail.com>
2023-01-13 00:55:32 +0200

    Memo. Add `CounterApp`

---
+++ tests/memo/src/main/kotlin/com/test/memo/CounterApp.kt
@@ -0,0 +1,17 @@
+package com.test.memo
+
+import react.VFC
+import react.dom.html.ReactHTML.div
+
+const val COUNTER_CONTAINER_ID = "counter-container"
+
+val CounterContainer = VFC {
+    val renderCount = useRenderCount()
+
+    div {
+        id = COUNTER_CONTAINER_ID
+        dataCount = renderCount
+
+        Counter()
+    }
+}
--- tests/memo/src/main/kotlin/com/test/memo/CounterContainer.kt
+++ tests/memo/src/main/kotlin/com/test/memo/CounterContainer.kt
@@ -1,15 +1,7 @@
 package com.test.memo
 
 import react.VFC
-import react.dom.html.ReactHTML.button
 
-const val COUNTER_CONTAINER_ID = "counter-container"
-
-val CounterContainer = VFC {
-    val renderCount = useRenderCount()
-
-    button {
-        id = COUNTER_CONTAINER_ID
-        dataCount = renderCount
-    }
+val CounterApp = VFC {
+    CounterContainer()
 }
