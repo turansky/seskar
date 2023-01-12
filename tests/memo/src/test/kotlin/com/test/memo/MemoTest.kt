b59f156d582c2cef18399f61c7500a55111413ad
Victor Turansky <victor.turansky@gmail.com>
2023-01-13 01:17:40 +0200

    Memo. Simple `MemoTest`

---
+++ tests/memo/src/test/kotlin/com/test/memo/MemoTest.kt
@@ -0,0 +1,30 @@
+package com.test.memo
+
+import js.core.get
+import kotlinx.coroutines.test.TestResult
+import react.dom.test.runReactTest
+import react.dom.test.simulateChange
+import web.html.HTML.div
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MemoTest {
+    @Test
+    fun initial(): TestResult = runReactTest(CounterApp) { container ->
+        val target = container.getElementsByTagName(div)[0]
+
+        assertEquals(0, target.dataCount, "Count #0")
+
+        target.simulateChange()
+        assertEquals(1, target.dataCount, "Count #1")
+
+        target.simulateChange()
+        assertEquals(2, target.dataCount, "Count #2")
+
+        target.simulateChange()
+        assertEquals(3, target.dataCount, "Count #3")
+
+        target.simulateChange()
+        assertEquals(4, target.dataCount, "Count #4")
+    }
+}
