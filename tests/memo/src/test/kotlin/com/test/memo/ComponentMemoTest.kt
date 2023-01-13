19318d73781ef25cfdc0752fe5eb12eba79f6f8f
Victor Turansky <victor.turansky@gmail.com>
2023-01-13 21:22:23 +0200

    Update memoization tests

--- tests/memo/src/test/kotlin/com/test/memo/ComponentMemoTest.kt
+++ tests/memo/src/test/kotlin/com/test/memo/ComponentMemoTest.kt
@@ -6,13 +6,11 @@ import react.dom.test.runReactTest
 import react.dom.test.simulateChange
 import web.html.HTML.button
 import web.html.HTML.div
-import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ComponentMemoTest {
     @Test
-    @Ignore
     fun initial(): TestResult = runReactTest(CounterApp) { container ->
         val target = container.getElementsByTagName(div)[0]
         val button = container.getElementsByTagName(button)[0]
@@ -29,11 +27,11 @@ class ComponentMemoTest {
         assertEquals(1, button.dataCount, "Count #3.2")
 
         button.simulateChange()
-        assertEquals(3, target.dataCount, "Count #4.1")
+        assertEquals(4, target.dataCount, "Count #4.1")
         assertEquals(2, button.dataCount, "Count #4.2")
 
         button.simulateChange()
-        assertEquals(3, target.dataCount, "Count #5.1")
+        assertEquals(5, target.dataCount, "Count #5.1")
         assertEquals(3, button.dataCount, "Count #5.2")
     }
 }
