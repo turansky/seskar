f96238c96941d70bc1c30893fb9c0a4c5f7b290c
Victor Turansky <victor.turansky@gmail.com>
2023-01-13 12:35:18 +0200

    Update `MemoTest`

--- tests/memo/src/test/kotlin/com/test/memo/MemoTest.kt
+++ tests/memo/src/test/kotlin/com/test/memo/MemoTest.kt
@@ -4,6 +4,7 @@ import js.core.get
 import kotlinx.coroutines.test.TestResult
 import react.dom.test.runReactTest
 import react.dom.test.simulateChange
+import web.html.HTML.button
 import web.html.HTML.div
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -12,19 +13,25 @@ class MemoTest {
     @Test
     fun initial(): TestResult = runReactTest(CounterApp) { container ->
         val target = container.getElementsByTagName(div)[0]
+        val button = container.getElementsByTagName(button)[0]
 
-        assertEquals(1, target.dataCount, "Count #1")
-
-        target.simulateChange()
-        assertEquals(2, target.dataCount, "Count #2")
-
-        target.simulateChange()
-        assertEquals(3, target.dataCount, "Count #3")
+        assertEquals(1, target.dataCount, "Count #1.1")
+        assertEquals(1, button.dataCount, "Count #1.2")
 
         target.simulateChange()
-        assertEquals(4, target.dataCount, "Count #4")
+        assertEquals(2, target.dataCount, "Count #2.1")
+        assertEquals(1, button.dataCount, "Count #2.2")
 
         target.simulateChange()
-        assertEquals(5, target.dataCount, "Count #5")
+        assertEquals(3, target.dataCount, "Count #3.1")
+        assertEquals(1, button.dataCount, "Count #3.2")
+
+        button.simulateChange()
+        assertEquals(3, target.dataCount, "Count #4.1")
+        assertEquals(2, button.dataCount, "Count #4.2")
+
+        button.simulateChange()
+        assertEquals(3, target.dataCount, "Count #5.1")
+        assertEquals(3, button.dataCount, "Count #5.2")
     }
 }
