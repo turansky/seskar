24a064956702019de67555e42e56ad68084030fe
Victor Turansky <victor.turansky@gmail.com>
2023-01-13 03:54:57 +0200

    Use `useUpdate`

--- tests/memo/src/main/kotlin/com/test/memo/CounterApp.kt
+++ tests/memo/src/main/kotlin/com/test/memo/CounterApp.kt
@@ -2,16 +2,23 @@ package com.test.memo
 
 import react.VFC
 import react.dom.html.ReactHTML.div
+import react.dom.onChange
 import react.use.useRenderCount
+import react.use.useUpdate
 
 const val COUNTER_CONTAINER_ID = "counter-container"
 
 val CounterContainer = VFC {
     val renderCount = useRenderCount()
+    val update = useUpdate()
 
     div {
         id = COUNTER_CONTAINER_ID
         dataCount = renderCount
+
+        onChange = {
+            update()
+        }
 
         Counter()
     }
