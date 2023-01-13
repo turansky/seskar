4a511fb665a90a560ec58ce6a55137b1f2f6ce4b
Victor Turansky <victor.turansky@gmail.com>
2023-01-13 02:53:18 +0200

    TODO for `useUpdate`

--- tests/memo/src/main/kotlin/react/use/useUpdate.kt
+++ tests/memo/src/main/kotlin/react/use/useUpdate.kt
@@ -1,8 +1,9 @@
-package com.test.memo
+package react.use
 
 import react.useCallback
 import react.useState
 
+// TODO: remove after wrappers update
 fun useUpdate(): () -> Unit {
     val (_, setTrigger) = useState(0)
     return useCallback {
