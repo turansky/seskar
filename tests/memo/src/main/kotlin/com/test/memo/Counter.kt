9fcce05ad1b10dd34249ee3f212e6946b48bfdfd
Victor Turansky <victor.turansky@gmail.com>
2023-01-13 21:24:57 +0200

    Memo. Fix counters

--- tests/memo/src/main/kotlin/com/test/memo/Counter.kt
+++ tests/memo/src/main/kotlin/com/test/memo/Counter.kt
@@ -3,7 +3,9 @@ package com.test.memo
 import react.FC
 import react.Props
 import react.dom.html.ReactHTML.button
+import react.dom.onChange
 import react.use.useRenderCount
+import react.use.useUpdate
 
 const val COUNTER_ID = "counter"
 
@@ -13,11 +15,15 @@ external interface CounterProps : Props {
 
 val Counter = FC<CounterProps> { props ->
     val renderCount = useRenderCount()
+    val update = useUpdate()
 
     button {
         id = COUNTER_ID
         title = props.title
-
         dataCount = renderCount
+
+        onChange = {
+            update()
+        }
     }
 }
--- tests/memo/src/main/kotlin/com/test/memo/VoidCounter.kt
+++ tests/memo/src/main/kotlin/com/test/memo/VoidCounter.kt
@@ -2,12 +2,19 @@ package com.test.memo
 
 import react.VFC
 import react.dom.html.ReactHTML.button
+import react.dom.onChange
 import react.use.useRenderCount
+import react.use.useUpdate
 
 val VoidCounter = VFC {
     val renderCount = useRenderCount()
+    val update = useUpdate()
 
     button {
         dataCount = renderCount
+
+        onChange = {
+            update()
+        }
     }
 }
