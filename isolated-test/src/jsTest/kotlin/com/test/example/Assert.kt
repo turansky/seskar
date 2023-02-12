cd4e069a8888c8f5a56706dff7659aca40f6905e
Victor Turansky <victor.turansky@gmail.com>
2023-02-12 05:49:31 +0200

    Move sources to `src/jsMain/kotlin` folder

--- isolated-test/src/jsTest/kotlin/com/test/example/Assert.kt
+++ isolated-test/src/jsTest/kotlin/com/test/example/Assert.kt
@@ -2,14 +2,14 @@ package com.test.example
 
 fun <T : Enum<T>> assertEquals(
     expected: String,
-    actual: T
+    actual: T,
 ) {
     kotlin.test.assertEquals<Any>(expected = expected, actual = actual)
 }
 
 fun <T : Enum<T>> assertEquals(
     expected: Int,
-    actual: T
+    actual: T,
 ) {
     kotlin.test.assertEquals<Any>(expected = expected, actual = actual)
 }
