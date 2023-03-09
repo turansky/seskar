7b37772a9ca03f985851e904799145ffc72b8ec5
Victor Turansky <victor.turansky@gmail.com>
2023-03-09 11:27:47 +0200

    Add tests for delegate version

--- tests/value/src/jsTest/kotlin/com/test/counter/ActiveCounterTest.kt
+++ tests/value/src/jsTest/kotlin/com/test/counter/ActiveCounterTest.kt
@@ -27,4 +27,23 @@ class ActiveCounterTest {
         target.simulateChange()
         assertEquals(4, target.dataCount, "Count #4")
     }
+
+    @Test
+    fun initialWithDelegate(): TestResult = runReactTest(ActiveCounterWithDelegate) { container ->
+        val target = container.getElementsByTagName(div)[0]
+
+        assertEquals(0, target.dataCount, "Count #0")
+
+        target.simulateChange()
+        assertEquals(1, target.dataCount, "Count #1")
+
+        target.simulateChange()
+        assertEquals(2, target.dataCount, "Count #2")
+
+        target.simulateChange()
+        assertEquals(3, target.dataCount, "Count #3")
+
+        target.simulateChange()
+        assertEquals(4, target.dataCount, "Count #4")
+    }
 }
--- tests/value/src/jsTest/kotlin/com/test/counter/DependencyTest.kt
+++ tests/value/src/jsTest/kotlin/com/test/counter/DependencyTest.kt
@@ -27,4 +27,23 @@ class DependencyTest {
         target.simulateChange()
         assertEquals(0, target.dataCount, "Count #4")
     }
+
+    @Test
+    fun initialWithDelegate(): TestResult = runReactTest(InactiveCounterWithDelegate) { container ->
+        val target = container.getElementsByTagName(div)[0]
+
+        assertEquals(0, target.dataCount, "Count #0")
+
+        target.simulateChange()
+        assertEquals(0, target.dataCount, "Count #1")
+
+        target.simulateChange()
+        assertEquals(0, target.dataCount, "Count #2")
+
+        target.simulateChange()
+        assertEquals(0, target.dataCount, "Count #3")
+
+        target.simulateChange()
+        assertEquals(0, target.dataCount, "Count #4")
+    }
 }
