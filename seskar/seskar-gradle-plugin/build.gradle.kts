062cddcc56f7ded444cf1beaf05dd32c160746bf
Victor Turansky <victor.turansky@gmail.com>
2023-03-18 00:22:08 +0200

    Cleanup gradle plugin configuration

--- seskar/seskar-gradle-plugin/build.gradle.kts
+++ seskar/seskar-gradle-plugin/build.gradle.kts
@@ -14,29 +14,26 @@ tasks.compileKotlin {
     kotlinOptions.allWarningsAsErrors = false
 }
 
+val REPO_URL = "https://github.com/turansky/seskar"
+
 gradlePlugin {
+    website.set(REPO_URL)
+    vcsUrl.set(REPO_URL)
+
     plugins {
         create("seskar") {
             id = "io.github.turansky.seskar"
+            displayName = "Seskar"
+            description = "Additions to Kotlin data classes"
             implementationClass = "seskar.gradle.plugin.SeskarGradleSubplugin"
+            tags.set(
+                listOf(
+                    "kotlin",
+                    "dataclass",
+                    "equals",
+                    "hashcode"
+                )
+            )
         }
-    }
-}
-
-val REPO_URL = "https://github.com/turansky/seskar"
-
-pluginBundle {
-    website = REPO_URL
-    vcsUrl = REPO_URL
-
-    plugins.getByName("seskar") {
-        displayName = "Seskar"
-        description = "Additions to Kotlin data classes"
-        tags = listOf(
-            "kotlin",
-            "dataclass",
-            "equals",
-            "hashcode"
-        )
     }
 }
