cea7ae2766ff03867760c7b0530516fc5b37950b
Victor Turansky <victor.turansky@gmail.com>
2023-03-18 19:09:30 +0200

    Legacy plugin adapter

--- seskar/seskar-gradle-plugin/build.gradle.kts
+++ seskar/seskar-gradle-plugin/build.gradle.kts
@@ -43,6 +43,8 @@ pluginBundle {
     vcsUrl = REPO_URL
 
     plugins.getByName("seskar") {
+        displayName = "Seskar"
+        description = "Additions to Kotlin data classes"
         tags = TAGS
     }
 }
