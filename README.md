6f957e386235fa8f13467d8fd2a3698db57f78e9
Victor Turansky <victor.turansky@gmail.com>
2023-03-18 03:37:49 +0200

    Version `1.1.0`

--- README.md
+++ README.md
@@ -16,12 +16,12 @@ To add Seskar to your project, you need to the following configuration to your p
 ```kotlin
 plugins {
     kotlin("js") version "1.8.10"
-    id("io.github.turansky.seskar") version "1.0.5"
+    id("io.github.turansky.seskar") version "1.1.0"
 }
 
 // browser target
 dependencies {
-    implementation("io.github.turansky.seskar:seskar-core:1.0.5")
+    implementation("io.github.turansky.seskar:seskar-core:1.1.0")
 }
 ```
 
--- isolated-test/build.gradle.kts
+++ isolated-test/build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     kotlin("multiplatform") version "1.8.10"
     id("io.github.turansky.kfc.application") version "6.8.3"
-    id("io.github.turansky.seskar") version "1.0.5"
+    id("io.github.turansky.seskar") version "1.1.0"
 }
 
 dependencies {
-    jsMainImplementation("io.github.turansky.seskar:seskar-core:1.0.5")
+    jsMainImplementation("io.github.turansky.seskar:seskar-core:1.1.0")
 
     jsTestImplementation(kotlin("test-js"))
 }
