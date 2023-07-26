8e6ee52a60dd8f566b76a098064d1df75ea3711f
Victor Turansky <victor.turansky@gmail.com>
2023-07-26 13:47:25 +0300

    Bump version

--- seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/KotlinPluginArtifact.kt
+++ seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/KotlinPluginArtifact.kt
@@ -6,5 +6,5 @@ internal val KOTLIN_PLUGIN_ARTIFACT: SubpluginArtifact
     get() = SubpluginArtifact(
         groupId = "io.github.turansky.seskar",
         artifactId = "seskar-compiler-plugin",
-        version = "1.13.1-SNAPSHOT",
+        version = "1.14.0-SNAPSHOT",
     )
