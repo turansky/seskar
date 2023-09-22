9449748c72a357a582b80778113e0f29b5435181
Victor Turansky <victor.turansky@gmail.com>
2023-09-22 14:04:53 +0300

    Key. Use call position (draft)

--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/key/backend/DefaultKeyTransformer.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/key/backend/DefaultKeyTransformer.kt
@@ -1,6 +1,7 @@
 package seskar.compiler.key.backend
 
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
+import org.jetbrains.kotlin.backend.wasm.ir2wasm.getSourceLocation
 import org.jetbrains.kotlin.ir.declarations.IrFile
 import org.jetbrains.kotlin.ir.expressions.IrCall
 import org.jetbrains.kotlin.ir.expressions.IrExpression
@@ -8,11 +9,13 @@ import org.jetbrains.kotlin.ir.expressions.impl.IrCallImpl
 import org.jetbrains.kotlin.ir.expressions.impl.IrCompositeImpl
 import org.jetbrains.kotlin.ir.expressions.impl.IrConstImpl
 import org.jetbrains.kotlin.ir.types.defaultType
+import org.jetbrains.kotlin.ir.util.getPackageFragment
 import org.jetbrains.kotlin.ir.util.hasAnnotation
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
 import org.jetbrains.kotlin.name.CallableId
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
+import org.jetbrains.kotlin.wasm.ir.source.location.SourceLocation
 
 private val ELEMENT_BUILDER = FqName("react.ElementBuilder")
 
@@ -25,15 +28,6 @@ private val SET_DEFAULT_KEY = CallableId(
 internal class DefaultKeyTransformer(
     private val context: IrPluginContext,
 ) : IrElementTransformerVoid() {
-    // use WeakMap instead?
-    private var keyIndex = 0
-
-    override fun visitFile(declaration: IrFile): IrFile {
-        keyIndex = 0
-
-        return super.visitFile(declaration)
-    }
-
     override fun visitCall(expression: IrCall): IrExpression {
         val keyCall = keyCall(expression)
         val originalCall = super.visitCall(expression)
@@ -62,6 +56,9 @@ internal class DefaultKeyTransformer(
 
         val setDefaultKey = context.referenceFunctions(SET_DEFAULT_KEY).single()
 
+        val key = getCallKey(expression)
+            ?: expression.hashCode()
+
         val call = IrCallImpl.fromSymbolOwner(
             startOffset = expression.startOffset,
             endOffset = expression.endOffset,
@@ -72,12 +69,23 @@ internal class DefaultKeyTransformer(
             startOffset = expression.startOffset,
             endOffset = expression.endOffset,
             type = context.symbols.string.defaultType,
-            value = "@rdk/${++keyIndex}/${expression.hashCode()}",
+            value = "@rdk/$key",
         )
 
         call.putValueArgument(0, dispatchReceiver)
         call.putValueArgument(1, defaultKey)
 
         return call
+    }
+
+    private fun getCallKey(expression: IrCall): String? {
+        val file = expression.getPackageFragment() as? IrFile
+            ?: return null
+
+        val location = expression.getSourceLocation(file.fileEntry)
+        if (location !is SourceLocation.Location)
+            return null
+
+        return "${location.line}_${location.column}"
     }
 }
