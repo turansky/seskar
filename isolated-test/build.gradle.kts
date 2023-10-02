f6dc5dff6478b134d16532424cabb983af1ff702
Victor Turansky <victor.turansky@gmail.com>
2023-10-03 01:05:44 +0300

    Version `2.2.0`

--- isolated-test/build.gradle.kts
+++ isolated-test/build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     kotlin("multiplatform") apply false
     id("io.github.turansky.kfc.application")
-    id("io.github.turansky.seskar") version "2.1.0"
+    id("io.github.turansky.seskar") version "2.2.0"
 }
 
 dependencies {
-    jsMainImplementation("io.github.turansky.seskar:seskar-core:2.1.0")
+    jsMainImplementation("io.github.turansky.seskar:seskar-core:2.2.0")
 
     jsTestImplementation(kotlin("test-js"))
 }
