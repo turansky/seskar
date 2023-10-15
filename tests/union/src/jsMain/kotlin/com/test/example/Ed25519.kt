c9af9f4161b9b705e023adbf3cdaf7483ef70ac5
Victor Turansky <victor.turansky@gmail.com>
2023-10-15 05:06:15 +0300

    Add simple object test

---
+++ tests/union/src/jsMain/kotlin/com/test/example/Ed25519.kt
@@ -0,0 +1,6 @@
+package com.test.example
+
+import seskar.js.JsValue
+
+@JsValue("Ed25519")
+external object Ed25519
---
+++ tests/union/src/jsTest/kotlin/com/test/example/TopObjectTest.kt
@@ -0,0 +1,12 @@
+package com.test.example
+
+import kotlin.test.Ignore
+import kotlin.test.Test
+
+@Ignore
+class TopObjectTest {
+    @Test
+    fun test() {
+        assertEquals("Ed25519", Ed25519)
+    }
+}
