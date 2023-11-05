d2a1ad6e9b1a03ca03dceaedb4e94c13ff741b3c
Victor Turansky <victor.turansky@gmail.com>
2023-11-05 15:08:59 +0200

    Add object value processing

--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/union/backend/UnionTransformer.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/union/backend/UnionTransformer.kt
@@ -2,16 +2,25 @@ package seskar.compiler.union.backend
 
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.backend.common.ir.addDispatchReceiver
+import org.jetbrains.kotlin.descriptors.ClassKind
+import org.jetbrains.kotlin.descriptors.Modality
 import org.jetbrains.kotlin.ir.IrStatement
 import org.jetbrains.kotlin.ir.builders.declarations.addGetter
-import org.jetbrains.kotlin.ir.declarations.IrClass
-import org.jetbrains.kotlin.ir.declarations.IrDeclarationWithName
-import org.jetbrains.kotlin.ir.declarations.IrProperty
-import org.jetbrains.kotlin.ir.declarations.createBlockBody
+import org.jetbrains.kotlin.ir.declarations.*
 import org.jetbrains.kotlin.ir.expressions.IrExpression
 import org.jetbrains.kotlin.ir.expressions.impl.IrConstImpl
 import org.jetbrains.kotlin.ir.expressions.impl.IrReturnImpl
+import org.jetbrains.kotlin.ir.symbols.impl.IrClassSymbolImpl
+import org.jetbrains.kotlin.ir.symbols.impl.IrPropertySymbolImpl
+import org.jetbrains.kotlin.ir.util.companionObject
+import org.jetbrains.kotlin.ir.util.isNonCompanionObject
+import org.jetbrains.kotlin.ir.util.isTopLevelDeclaration
+import org.jetbrains.kotlin.ir.util.parentAsClass
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformer
+import org.jetbrains.kotlin.name.Name
+
+private object SYNTHETIC_UNION_COMPANION : IrDeclarationOriginImpl("SYNTHETIC_UNION_COMPANION", isSynthetic = true)
+private object SYNTHETIC_UNION_PROPERTY : IrDeclarationOriginImpl("SYNTHETIC_UNION_PROPERTY", isSynthetic = true)
 
 internal class UnionTransformer(
     private val context: IrPluginContext,
@@ -21,7 +30,8 @@ internal class UnionTransformer(
         data: ValueMode?,
     ): IrStatement {
         val value = declaration.value()
-        if (value != null) {
+
+        if (value != null && declaration.isTopLevelDeclaration && declaration.kind == ClassKind.OBJECT) {
             declaration.annotations += JsName(context, declaration, value.toJsName())
             return declaration
         }
@@ -33,13 +43,73 @@ internal class UnionTransformer(
             data == ValueMode.ROOT && declaration.isCompanion
             -> ValueMode.COMPANION
 
+            data == ValueMode.ROOT && declaration.isNonCompanionObject
+            -> return visitUnionEntry(declaration)
+
             else -> null
         }
 
-        if (mode == ValueMode.ROOT)
+        if (mode == ValueMode.ROOT) {
             declaration.annotations += JsName(context, declaration, "0")
+            checkCompanionObject(declaration)
+        }
 
         return super.visitClass(declaration, mode)
+    }
+
+    private fun checkCompanionObject(
+        union: IrClass,
+    ) {
+        if (union.companionObject() != null)
+            return
+
+        val companion = context.irFactory.createClass(
+            startOffset = union.startOffset,
+            endOffset = union.endOffset,
+            origin = SYNTHETIC_UNION_COMPANION,
+            // constant?
+            name = Name.identifier("Companion"),
+            visibility = union.visibility,
+            symbol = IrClassSymbolImpl(),
+            kind = ClassKind.OBJECT,
+            modality = Modality.FINAL,
+            isExternal = true,
+            isCompanion = true,
+        )
+
+        union.declarations.add(companion)
+        companion.parent = union
+    }
+
+    private fun visitUnionEntry(
+        unionEntry: IrClass,
+    ): IrClass {
+        unionEntry.kind = ClassKind.INTERFACE
+        unionEntry.declarations.clear()
+
+        val companion = unionEntry.parentAsClass.companionObject()
+            ?: error("No companion!")
+
+        val property = context.irFactory.createProperty(
+            startOffset = unionEntry.startOffset,
+            endOffset = unionEntry.endOffset,
+            origin = SYNTHETIC_UNION_PROPERTY,
+            name = unionEntry.name,
+            visibility = unionEntry.visibility,
+            modality = unionEntry.modality,
+            symbol = IrPropertySymbolImpl(),
+            isVar = false,
+            isConst = false,
+            isLateinit = false,
+            isDelegated = false,
+        )
+
+        property.parent = companion
+        companion.declarations.add(property)
+
+        addPropertyGetter(property, unionEntry.value(useDefaultValue = true)!!)
+
+        return unionEntry
     }
 
     override fun visitProperty(
@@ -49,6 +119,15 @@ internal class UnionTransformer(
         val value = declaration.value(useDefaultValue = (data == ValueMode.COMPANION))
             ?: return declaration
 
+        addPropertyGetter(declaration, value)
+
+        return super.visitProperty(declaration, data)
+    }
+
+    private fun addPropertyGetter(
+        declaration: IrProperty,
+        value: Value,
+    ) {
         val getter = declaration.addGetter {
             isInline = true
             returnType = context.irBuiltIns.stringType
@@ -71,8 +150,6 @@ internal class UnionTransformer(
                 )
             )
         )
-
-        return super.visitProperty(declaration, data)
     }
 
     private fun valueConstant(
