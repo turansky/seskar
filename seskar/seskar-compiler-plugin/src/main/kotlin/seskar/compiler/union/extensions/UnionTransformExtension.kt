ce4d147c9d87ee41b77e9191c32da7d8cd715cb1
Victor Turansky <victor.turansky@gmail.com>
2023-11-08 00:35:55 +0200

    `UnionInlineTransformer` removed

--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/union/extensions/UnionTransformExtension.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/union/extensions/UnionTransformExtension.kt
@@ -4,7 +4,6 @@ import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.ir.visitors.transformChildrenVoid
-import seskar.compiler.union.backend.UnionInlineTransformer
 import seskar.compiler.union.backend.UnionTransformer
 
 internal class UnionTransformExtension : IrGenerationExtension {
@@ -13,6 +12,5 @@ internal class UnionTransformExtension : IrGenerationExtension {
         pluginContext: IrPluginContext,
     ) {
         moduleFragment.transformChildrenVoid(UnionTransformer(pluginContext))
-        moduleFragment.transformChildrenVoid(UnionInlineTransformer(pluginContext))
     }
 }
