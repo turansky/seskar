02925da8d2c1ae1f9c7e06f8a1d7f3e294589665
Victor Turansky <victor.turansky@gmail.com>
2023-11-07 03:55:24 +0200

    Add `LocalAlignTest`

---
+++ tests/union/src/jsTest/kotlin/com/test/example/LocalAlign.kt
@@ -0,0 +1,25 @@
+@file:Suppress(
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
+package com.test.example
+
+import seskar.js.JsUnion
+import seskar.js.JsValue
+
+@JsUnion
+sealed external interface LocalAlign {
+    companion object {
+        @JsValue("TOP")
+        val TOP: LocalAlign
+
+        @JsValue("LEFT")
+        val LEFT: LocalAlign
+
+        @JsValue("BOTTOM")
+        val BOTTOM: LocalAlign
+
+        @JsValue("RIGHT")
+        val RIGHT: LocalAlign
+    }
+}
---
+++ tests/union/src/jsTest/kotlin/com/test/example/LocalAlignTest.kt
@@ -0,0 +1,13 @@
+package com.test.example
+
+import kotlin.test.Test
+
+class LocalAlignTest {
+    @Test
+    fun test() {
+        assertEquals("TOP", LocalAlign.TOP)
+        assertEquals("LEFT", LocalAlign.LEFT)
+        assertEquals("BOTTOM", LocalAlign.BOTTOM)
+        assertEquals("RIGHT", LocalAlign.RIGHT)
+    }
+}
