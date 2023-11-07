bebbec41fbcdb12272526f517cc0b55765a9af36
Victor Turansky <victor.turansky@gmail.com>
2023-11-07 02:15:04 +0200

    Add required values

--- tests/union/src/jsMain/kotlin/com/test/example/Align.kt
+++ tests/union/src/jsMain/kotlin/com/test/example/Align.kt
@@ -5,13 +5,21 @@
 package com.test.example
 
 import seskar.js.JsUnion
+import seskar.js.JsValue
 
 @JsUnion
 sealed external interface Align {
     companion object {
+        @JsValue("TOP")
         val TOP: Align
+
+        @JsValue("LEFT")
         val LEFT: Align
+
+        @JsValue("BOTTOM")
         val BOTTOM: Align
+
+        @JsValue("RIGHT")
         val RIGHT: Align
     }
 }
