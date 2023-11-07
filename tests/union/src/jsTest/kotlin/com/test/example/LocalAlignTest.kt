04fb2d8f4448c425a9cb9bb4e532b784376af4d8
Victor Turansky <victor.turansky@gmail.com>
2023-11-07 14:15:30 +0200

    Revert "Ignore `LocalAlignTest`"
    This reverts commit c1b73719d2030b1182d4f4c7f2335f41e7e1384d.

--- tests/union/src/jsTest/kotlin/com/test/example/LocalAlignTest.kt
+++ tests/union/src/jsTest/kotlin/com/test/example/LocalAlignTest.kt
@@ -5,11 +5,9 @@ import kotlin.test.Test
 class LocalAlignTest {
     @Test
     fun test() {
-        /*
         assertEquals("TOP", LocalAlign.TOP)
         assertEquals("LEFT", LocalAlign.LEFT)
         assertEquals("BOTTOM", LocalAlign.BOTTOM)
         assertEquals("RIGHT", LocalAlign.RIGHT)
-        */
     }
 }
