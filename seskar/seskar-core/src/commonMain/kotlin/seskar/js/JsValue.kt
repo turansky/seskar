d03939d877a7ac1f5f7a684079fd0e40adfe182f
Victor Turansky <victor.turansky@gmail.com>
2023-11-08 21:31:00 +0200

    Split annotation files

--- seskar/seskar-core/src/commonMain/kotlin/seskar/js/JsValue.kt
+++ seskar/seskar-core/src/commonMain/kotlin/seskar/js/JsValue.kt
@@ -1,9 +1,5 @@
 package seskar.js
 
-@Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.BINARY)
-annotation class JsVirtual
-
 @Target(AnnotationTarget.PROPERTY)
 @Retention(AnnotationRetention.BINARY)
 annotation class JsIntValue(
