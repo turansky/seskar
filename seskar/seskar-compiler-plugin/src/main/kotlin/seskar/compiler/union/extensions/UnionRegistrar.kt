480d7405e5311b80ac91082b59f68b0a35bd7654
Victor Turansky <victor.turansky@gmail.com>
2023-11-10 21:43:58 +0200

    Restore `UnionRegistrar`

--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/union/extensions/UnionRegistrar.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/union/extensions/UnionRegistrar.kt
@@ -3,7 +3,6 @@ package seskar.compiler.union.extensions
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
 import org.jetbrains.kotlin.config.CompilerConfiguration
-import seskar.compiler.specialname.extensions.SpecialNameTransformExtension
 
 class UnionRegistrar : CompilerPluginRegistrar() {
     override val supportsK2: Boolean = true
@@ -11,6 +10,6 @@ class UnionRegistrar : CompilerPluginRegistrar() {
     override fun ExtensionStorage.registerExtensions(
         configuration: CompilerConfiguration,
     ) {
-        IrGenerationExtension.registerExtension(SpecialNameTransformExtension())
+        IrGenerationExtension.registerExtension(UnionTransformExtension())
     }
 }
--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/union/extensions/UnionTransformExtension.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/union/extensions/UnionTransformExtension.kt
@@ -4,13 +4,13 @@ import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.ir.visitors.transformChildrenVoid
-import seskar.compiler.specialname.backend.SpecialNameTransformer
+import seskar.compiler.union.backend.UnionTransformer
 
 internal class UnionTransformExtension : IrGenerationExtension {
     override fun generate(
         moduleFragment: IrModuleFragment,
         pluginContext: IrPluginContext,
     ) {
-        moduleFragment.transformChildrenVoid(SpecialNameTransformer(pluginContext))
+        moduleFragment.transformChildrenVoid(UnionTransformer(pluginContext))
     }
 }
