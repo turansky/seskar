db9791360b994485f5f269e5bfcb4b46663675de
Victor Turansky <victor.turansky@gmail.com>
2023-11-24 22:03:24 +0200

    Version `2.15.0`

--- isolated-test/build.gradle.kts
+++ isolated-test/build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     kotlin("multiplatform") apply false
     id("io.github.turansky.kfc.application")
-    id("io.github.turansky.seskar") version "2.14.1"
+    id("io.github.turansky.seskar") version "2.15.0"
 }
 
 dependencies {
-    jsMainImplementation("io.github.turansky.seskar:seskar-core:2.14.1")
+    jsMainImplementation("io.github.turansky.seskar:seskar-core:2.15.0")
 
     jsTestImplementation(kotlin("test-js"))
 }
--- seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/KotlinPluginArtifact.kt
+++ seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/KotlinPluginArtifact.kt
@@ -6,5 +6,5 @@ internal val KOTLIN_PLUGIN_ARTIFACT: SubpluginArtifact
     get() = SubpluginArtifact(
         groupId = "io.github.turansky.seskar",
         artifactId = "seskar-compiler-plugin",
-        version = "2.14.2-SNAPSHOT",
+        version = "2.15.1-SNAPSHOT",
     )
