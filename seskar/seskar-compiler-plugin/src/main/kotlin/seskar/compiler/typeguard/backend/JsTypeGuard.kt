0112db6a074be83f9fb02a87efdc56ad87cd6145
Victor Turansky <victor.turansky@gmail.com>
2023-12-08 02:08:18 +0200

    Add guard data calculation

--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/typeguard/backend/JsTypeGuard.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/typeguard/backend/JsTypeGuard.kt
@@ -6,16 +6,24 @@ import org.jetbrains.kotlin.ir.expressions.IrConstructorCall
 import org.jetbrains.kotlin.ir.util.getAnnotation
 import org.jetbrains.kotlin.name.FqName
 
+internal class TypeGuardData(
+    val property: String,
+    val value: String,
+)
+
 private val JS_TYPE_GUARD = FqName("seskar.js.JsTypeGuard")
 
-private fun IrConstructorCall.value(): String {
-    val argument = getValueArgument(0) as IrConst<*>
+private fun IrConstructorCall.value(index: Int): String {
+    val argument = getValueArgument(index) as IrConst<*>
     return argument.value as String
 }
 
-internal fun IrProperty.specialName(): String? {
-    val name = getAnnotation(JS_TYPE_GUARD)
+internal fun IrProperty.typeGuardData(): TypeGuardData? {
+    val guard = getAnnotation(JS_TYPE_GUARD)
         ?: return null
 
-    return name.value()
+    return TypeGuardData(
+        property = guard.value(0),
+        value = guard.value(1),
+    )
 }
