31c120b2b48133de135889656c9cbd3512594534
Victor Turansky <victor.turansky@gmail.com>
2023-12-09 19:18:05 +0200

    Update guard constructing

--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/typeguard/backend/TypeGuardTransformer.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/typeguard/backend/TypeGuardTransformer.kt
@@ -8,21 +8,18 @@ import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
 import seskar.compiler.common.backend.JsName
 import seskar.compiler.common.backend.JsQualifier
 
-private const val GUARD_NAME: String = "__guard__"
+private const val CONSTRUCTOR: String = "constructor"
 
 private fun guard(
     name: String,
     data: TypeGuardData,
 ): String =
     // language=javascript
-    """({
-    $GUARD_NAME: {
-        name: '$name',
-        [Symbol.hasInstance](instance) {
-            return instance && (typeof instance === 'object') && (instance.${data.property} === ${data.value})
-        }
+    """(class $name {
+    static [Symbol.hasInstance](instance) {
+        return instance && (typeof instance === 'object') && (instance.${data.property} === ${data.value})
     }
-    })"""
+    }.prototype)"""
 
 internal class TypeGuardTransformer(
     private val context: IrPluginContext,
@@ -31,7 +28,7 @@ internal class TypeGuardTransformer(
         val data = declaration.typeGuardData()
         if (data != null) {
             declaration.file.annotations += JsQualifier(context, guard(declaration.name.asString(), data))
-            declaration.annotations += JsName(context, GUARD_NAME)
+            declaration.annotations += JsName(context, CONSTRUCTOR)
         }
 
         return declaration
