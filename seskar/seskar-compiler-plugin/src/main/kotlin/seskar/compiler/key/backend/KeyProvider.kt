97d46daeaaf1f24bad3278261aa9e375f68741e8
Victor Turansky <victor.turansky@gmail.com>
2024-01-19 10:26:28 +0200

    Inline location calculation

--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/key/backend/KeyProvider.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/key/backend/KeyProvider.kt
@@ -1,20 +1,20 @@
 package seskar.compiler.key.backend
 
-import org.jetbrains.kotlin.backend.wasm.ir2wasm.getSourceLocation
 import org.jetbrains.kotlin.ir.IrFileEntry
 import org.jetbrains.kotlin.ir.expressions.IrCall
-import org.jetbrains.kotlin.wasm.ir.source.location.SourceLocation
 
 @JvmInline
 internal value class KeyProvider(
     private val fileEntry: IrFileEntry,
 ) {
     fun get(expression: IrCall): String {
-        val location = expression.getSourceLocation(fileEntry)
-        require(location is SourceLocation.Location) {
+        val startLine = fileEntry.getLineNumber(expression.startOffset)
+        val startColumn = fileEntry.getColumnNumber(expression.startOffset)
+
+        require(startLine >= 0 && startColumn >= 0) {
             "Invalid call location"
         }
 
-        return "${location.line}_${location.column}"
+        return "${startLine}_${startColumn}"
     }
 }
