3329421160736d788b9f7e54abda807b14f2c127
Victor Turansky <victor.turansky@gmail.com>
2024-02-26 01:55:41 +0200

    Wrappers `pre.703`

--- tests/union/src/jsMain/kotlin/com/test/example/MouseEvent.kt
+++ tests/union/src/jsMain/kotlin/com/test/example/MouseEvent.kt
@@ -2,11 +2,12 @@ package com.test.example
 
 import seskar.js.JsValue
 import web.events.Event
+import web.events.EventTarget
 import web.events.EventType
 
-external class MouseEvent : Event {
+external class MouseEvent<C : EventTarget?> : Event<C> {
     companion object {
         @JsValue("click")
-        val CLICK: EventType<MouseEvent>
+        val CLICK: EventType<MouseEvent<*>>
     }
 }
