311b138712f4df8e984793142998170cda92a533
Victor Turansky <victor.turansky@gmail.com>
2024-02-26 00:35:58 +0200

    Function target for `@JsValue`

--- seskar/seskar-core/src/commonMain/kotlin/seskar/js/JsValue.kt
+++ seskar/seskar-core/src/commonMain/kotlin/seskar/js/JsValue.kt
@@ -1,6 +1,9 @@
 package seskar.js
 
-@Target(AnnotationTarget.PROPERTY)
+@Target(
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.FUNCTION,
+)
 @Retention(AnnotationRetention.BINARY)
 annotation class JsIntValue(
     val value: Int,
@@ -9,6 +12,7 @@ annotation class JsIntValue(
 @Target(
     AnnotationTarget.CLASS,
     AnnotationTarget.PROPERTY,
+    AnnotationTarget.FUNCTION,
 )
 @Retention(AnnotationRetention.BINARY)
 annotation class JsValue(
