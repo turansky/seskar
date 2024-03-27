4956c448a1a706514864da08763f533d27215063
Victor Turansky <victor.turansky@gmail.com>
2024-03-27 23:01:22 +0200

    Special name support for function

--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/specialname/backend/SpecialNameTransformer.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/specialname/backend/SpecialNameTransformer.kt
@@ -3,6 +3,8 @@ package seskar.compiler.specialname.backend
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.ir.IrStatement
 import org.jetbrains.kotlin.ir.declarations.IrClass
+import org.jetbrains.kotlin.ir.declarations.IrDeclarationBase
+import org.jetbrains.kotlin.ir.declarations.IrFunction
 import org.jetbrains.kotlin.ir.declarations.IrProperty
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
 import seskar.compiler.common.backend.JsName
@@ -10,11 +12,10 @@ import seskar.compiler.common.backend.JsName
 internal class SpecialNameTransformer(
     private val context: IrPluginContext,
 ) : IrElementTransformerVoid() {
-    override fun visitClass(declaration: IrClass): IrStatement {
-        val specialName = declaration.specialName()
-        if (specialName != null) {
-            declaration.annotations += JsName(context, specialName)
-        }
+    override fun visitClass(
+        declaration: IrClass,
+    ): IrStatement {
+        processSpecialName(declaration)
 
         return super.visitClass(declaration)
     }
@@ -22,11 +23,25 @@ internal class SpecialNameTransformer(
     override fun visitProperty(
         declaration: IrProperty,
     ): IrStatement {
+        processSpecialName(declaration)
+
+        return declaration
+    }
+
+    override fun visitFunction(
+        declaration: IrFunction,
+    ): IrStatement {
+        processSpecialName(declaration)
+
+        return super.visitFunction(declaration)
+    }
+
+    private fun processSpecialName(
+        declaration: IrDeclarationBase,
+    ) {
         val specialName = declaration.specialName()
         if (specialName != null) {
             declaration.annotations += JsName(context, specialName)
         }
-
-        return declaration
     }
 }
--- seskar/seskar-core/src/commonMain/kotlin/seskar/js/JsSpecialName.kt
+++ seskar/seskar-core/src/commonMain/kotlin/seskar/js/JsSpecialName.kt
@@ -3,6 +3,7 @@ package seskar.js
 @Target(
     AnnotationTarget.CLASS,
     AnnotationTarget.PROPERTY,
+    AnnotationTarget.FUNCTION,
 )
 @Retention(AnnotationRetention.BINARY)
 annotation class JsSpecialName(
