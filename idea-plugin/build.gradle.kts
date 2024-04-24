4c7093cde6696b9bf573ad4c08c4df2a95e3d0b4
Victor Turansky <victor.turansky@gmail.com>
2024-04-25 01:03:37 +0300

    IP. Build script

---
+++ idea-plugin/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    kotlin("jvm") version "1.9.23"
+    id("org.jetbrains.intellij") version "1.17.3"
+    id("io.github.turansky.kfc.version") version "7.58.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+intellij {
+    pluginName = "seskar"
+
+    type = "IU"
+    version = "2024.1"
+
+    plugins = listOf(
+        "java",
+        "org.jetbrains.kotlin",
+        "JavaScript"
+    )
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    runIde {
+        jvmArgs(
+            "-Xms1g",
+            "-Xmx4g"
+        )
+    }
+
+    patchPluginXml {
+        sinceBuild = "241.*"
+        untilBuild = "242.*"
+    }
+
+    publishPlugin {
+        token = project.property("intellij.publish.token") as String
+    }
+
+    buildSearchableOptions {
+        enabled = false
+    }
+
+    wrapper {
+        gradleVersion = "8.7"
+    }
+}
