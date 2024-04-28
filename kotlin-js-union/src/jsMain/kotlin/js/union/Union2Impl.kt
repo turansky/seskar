c1a9c2c4b81d2c43bff8f9586bb004cb2952f97d
Victor Turansky <victor.turansky@gmail.com>
2024-04-28 21:42:11 +0300

    Add `Union2Impl` class

---
+++ kotlin-js-union/src/jsMain/kotlin/js/union/Union2Impl.kt
@@ -0,0 +1,18 @@
+package js.union
+
+internal class Union2Impl<in A : Any, in B : Any>(
+    private val target: Any,
+    private val propertyName: String,
+) : Union2<A, B> {
+    override fun assign(
+        value: A,
+    ) {
+        target.asDynamic()[propertyName] = value
+    }
+
+    override fun assign(
+        value: B,
+    ) {
+        target.asDynamic()[propertyName] = value
+    }
+}
