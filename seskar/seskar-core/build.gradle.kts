2b79be87f9dccf17febecf93d3920694bb9d9d5b
Victor Turansky <victor.turansky@gmail.com>
2024-04-28 20:21:31 +0300

    Remove redundant targets

--- seskar/seskar-core/build.gradle.kts
+++ seskar/seskar-core/build.gradle.kts
@@ -2,11 +2,3 @@ plugins {
     id("io.github.turansky.kfc.library")
     id("io.github.turansky.kfc.maven-central-publish")
 }
-
-kotlin {
-    js()
-    jvm()
-    iosArm64()
-    iosX64()
-    iosSimulatorArm64()
-}
