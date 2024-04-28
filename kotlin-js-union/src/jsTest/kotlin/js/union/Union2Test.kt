212554dc109098aa26962849497c2c508ccb17c7
Victor Turansky <victor.turansky@gmail.com>
2024-04-29 00:54:07 +0300

    Union. Simple test

--- kotlin-js-union/src/jsTest/kotlin/js/union/Union2Test.kt
+++ kotlin-js-union/src/jsTest/kotlin/js/union/Union2Test.kt
@@ -1,11 +1,20 @@
 package js.union
 
 import kotlin.test.Test
+import kotlin.test.assertEquals
+
+private class A {
+    val a: Union2<String, Int>
+        get() = Union2Impl(this, "aaa")
+}
 
 class Union2Test {
 
     @Test
-    fun assignA() {
+    fun assignPropertyToString() {
+        val a = A()
+        a.a = "aaaValue"
 
+        assertEquals(a.asDynamic()["aaa"], "aaaValue")
     }
 }
