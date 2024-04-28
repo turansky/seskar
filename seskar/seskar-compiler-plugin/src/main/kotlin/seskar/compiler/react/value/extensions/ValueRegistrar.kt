b61f6ed144f9fc0cde7356c876b505f92396b0de
Victor Turansky <victor.turansky@gmail.com>
2024-04-28 21:12:04 +0300

    Cleanup

--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/react/value/extensions/ValueRegistrar.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/react/value/extensions/ValueRegistrar.kt
@@ -3,7 +3,6 @@ package seskar.compiler.react.value.extensions
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
 import org.jetbrains.kotlin.config.CompilerConfiguration
-import org.jetbrains.kotlin.extensions.StorageComponentContainerContributor
 
 class ValueRegistrar : CompilerPluginRegistrar() {
     override val supportsK2: Boolean = true
@@ -11,10 +10,6 @@ class ValueRegistrar : CompilerPluginRegistrar() {
     override fun ExtensionStorage.registerExtensions(
         configuration: CompilerConfiguration,
     ) {
-        StorageComponentContainerContributor.registerExtension(
-            extension = ValueStorageComponentContainerContributor(),
-        )
-
         IrGenerationExtension.registerExtension(ValueTransformExtension())
     }
 }
