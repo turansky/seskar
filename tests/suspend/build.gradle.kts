0f349386854df6f2e2f23a556d42f6148dca38c9
Victor Turansky <victor.turansky@gmail.com>
2024-05-03 18:02:44 +0300

    Suspend. Add coroutines depoendency

--- tests/suspend/build.gradle.kts
+++ tests/suspend/build.gradle.kts
@@ -4,9 +4,13 @@ plugins {
     id("io.github.turansky.seskar")
 }
 
+val coroutinesVersion = project.property("kotlinx-coroutines.version") as String
+
 dependencies {
     jsMainImplementation("io.github.turansky.seskar:seskar-core")
     jsMainImplementation(wrappers("js"))
 
     jsTestImplementation(kotlin("test-js"))
+    jsTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:$coroutinesVersion")
+    jsTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
 }
