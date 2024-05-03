159b2ea668a3d00ace7a1e72a0073edc536866f6
Victor Turansky <victor.turansky@gmail.com>
2024-05-03 18:21:46 +0300

    Suspend. Configure `getResponse` method

--- tests/suspend/src/jsTest/kotlin/com/test/example/GlobalThisTest.kt
+++ tests/suspend/src/jsTest/kotlin/com/test/example/GlobalThisTest.kt
@@ -1,12 +1,22 @@
 package com.test.example
 
+import js.globals.globalThis
+import js.promise.Promise
 import kotlinx.coroutines.test.runTest
+import kotlin.test.BeforeTest
 import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 @Ignore
 class GlobalThisTest {
+    @BeforeTest
+    fun setUp() {
+        globalThis.getResponse = {
+            Promise.resolve(42)
+        }
+    }
+
     @Test
     fun testGetResources() = runTest {
         val data = localGlobalThis.getResponse("http://github.com")
