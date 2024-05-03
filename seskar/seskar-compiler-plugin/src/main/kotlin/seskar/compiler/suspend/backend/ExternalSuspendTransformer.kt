72c00df87431651a91787b7e96fa6429c3733b0a
Victor Turansky <victor.turansky@gmail.com>
2024-05-04 01:11:40 +0300

    Suspend. Generate inline `suspend` call

--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/suspend/backend/ExternalSuspendTransformer.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/suspend/backend/ExternalSuspendTransformer.kt
@@ -4,10 +4,24 @@ import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.ir.IrStatement
 import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrFunction
+import org.jetbrains.kotlin.ir.declarations.createBlockBody
+import org.jetbrains.kotlin.ir.expressions.IrCall
+import org.jetbrains.kotlin.ir.expressions.IrExpression
+import org.jetbrains.kotlin.ir.expressions.impl.IrCallImpl
+import org.jetbrains.kotlin.ir.expressions.impl.IrReturnImpl
+import org.jetbrains.kotlin.ir.symbols.IrSimpleFunctionSymbol
 import org.jetbrains.kotlin.ir.util.isSuspend
 import org.jetbrains.kotlin.ir.util.isTopLevel
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
+import org.jetbrains.kotlin.name.CallableId
+import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.name.Name
 import seskar.compiler.common.backend.isReallyExternal
+
+private val AWAIT = CallableId(
+    packageName = FqName("js.promise"),
+    callableName = Name.identifier("await"),
+)
 
 internal class ExternalSuspendTransformer(
     private val context: IrPluginContext,
@@ -32,6 +46,57 @@ internal class ExternalSuspendTransformer(
             return declaration
         }
 
+        addFunctionBody(declaration)
+
         return declaration
+    }
+
+    private fun addFunctionBody(
+        declaration: IrFunction,
+    ) {
+        declaration.isInline = true
+        declaration.body = context.irFactory.createBlockBody(
+            startOffset = declaration.startOffset,
+            endOffset = declaration.endOffset,
+            statements = listOf(
+                IrReturnImpl(
+                    startOffset = declaration.startOffset,
+                    endOffset = declaration.endOffset,
+                    type = declaration.returnType,
+                    returnTargetSymbol = declaration.symbol,
+                    value = suspendCall(declaration),
+                )
+            )
+        )
+    }
+
+    private fun suspendCall(
+        declaration: IrFunction,
+    ): IrExpression {
+        val function = findAsyncFunction(declaration)
+
+        val promiseCall = IrCallImpl.fromSymbolOwner(
+            startOffset = declaration.startOffset,
+            endOffset = declaration.endOffset,
+            symbol = function.symbol as IrSimpleFunctionSymbol,
+        )
+
+        return await(promiseCall)
+    }
+
+    private fun await(
+        promiseCall: IrCall,
+    ): IrExpression {
+        val await = context.referenceFunctions(AWAIT).single()
+
+        val call = IrCallImpl.fromSymbolOwner(
+            startOffset = promiseCall.startOffset,
+            endOffset = promiseCall.endOffset,
+            symbol = await,
+        )
+
+        call.extensionReceiver = promiseCall
+
+        return call
     }
 }
