33d4bd433c0841c2fec6b15c12bef73a936ef0ad
Victor Turansky <victor.turansky@gmail.com>
2024-05-04 03:42:06 +0300

    Add `irCall` function

--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/common/backend/Ir.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/common/backend/Ir.kt
@@ -2,8 +2,11 @@ package seskar.compiler.common.backend
 
 import org.jetbrains.kotlin.ir.UNDEFINED_OFFSET
 import org.jetbrains.kotlin.ir.declarations.IrValueParameter
+import org.jetbrains.kotlin.ir.expressions.IrCall
 import org.jetbrains.kotlin.ir.expressions.IrGetValue
+import org.jetbrains.kotlin.ir.expressions.impl.IrCallImpl
 import org.jetbrains.kotlin.ir.expressions.impl.IrGetValueImpl
+import org.jetbrains.kotlin.ir.symbols.IrSimpleFunctionSymbol
 
 internal fun irGet(
     parameter: IrValueParameter,
@@ -12,4 +15,13 @@ internal fun irGet(
         startOffset = UNDEFINED_OFFSET,
         endOffset = UNDEFINED_OFFSET,
         symbol = parameter.symbol,
+    )
+
+internal fun irCall(
+    symbol: IrSimpleFunctionSymbol,
+): IrCall =
+    IrCallImpl.fromSymbolOwner(
+        startOffset = UNDEFINED_OFFSET,
+        endOffset = UNDEFINED_OFFSET,
+        symbol = symbol,
     )
