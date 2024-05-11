aa4501d2da561bd2f0c5fe939791cce7bf46a75c
Victor Turansky <victor.turansky@gmail.com>
2024-05-12 02:39:12 +0300

    Check external suspend functions

--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/suspend/backend/ExternalSuspendTransformer.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/suspend/backend/ExternalSuspendTransformer.kt
@@ -9,8 +9,10 @@ import org.jetbrains.kotlin.ir.declarations.createBlockBody
 import org.jetbrains.kotlin.ir.expressions.IrCall
 import org.jetbrains.kotlin.ir.expressions.IrExpression
 import org.jetbrains.kotlin.ir.expressions.impl.IrReturnImpl
+import org.jetbrains.kotlin.ir.util.hasAnnotation
 import org.jetbrains.kotlin.ir.util.isSuspend
 import org.jetbrains.kotlin.ir.util.isTopLevel
+import org.jetbrains.kotlin.ir.util.kotlinFqName
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
 import org.jetbrains.kotlin.name.CallableId
 import org.jetbrains.kotlin.name.FqName
@@ -18,6 +20,8 @@ import org.jetbrains.kotlin.name.Name
 import seskar.compiler.common.backend.irCall
 import seskar.compiler.common.backend.irGet
 import seskar.compiler.common.backend.isReallyExternal
+
+private val JS_ASYNC = FqName("seskar.js.JsAsync")
 
 private val AWAIT_PROMISE_LIKE = CallableId(
     packageName = FqName("js.promise.internal"),
@@ -49,10 +53,15 @@ internal class ExternalSuspendTransformer(
             !declaration.isInline
         }
 
-        if (useTransform) {
-            addFunctionBody(declaration)
+        if (!useTransform) {
+            return declaration
         }
 
+        require(declaration.hasAnnotation(JS_ASYNC)) {
+            "Missed @JsAsync annotation for `${declaration.kotlinFqName.asString()}`"
+        }
+
+        addFunctionBody(declaration)
         return declaration
     }
 
