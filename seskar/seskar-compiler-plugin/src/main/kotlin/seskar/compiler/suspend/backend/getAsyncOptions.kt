365e850a7a59a1998a91b84d61d826aa880d7aeb
Victor Turansky <victor.turansky@gmail.com>
2024-05-12 22:36:26 +0300

    Respect `JsAsync.optional` flag

--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/suspend/backend/getAsyncOptions.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/suspend/backend/getAsyncOptions.kt
@@ -1,16 +1,36 @@
 package seskar.compiler.suspend.backend
 
 import org.jetbrains.kotlin.ir.declarations.IrFunction
-import org.jetbrains.kotlin.ir.util.hasAnnotation
+import org.jetbrains.kotlin.ir.expressions.IrConst
+import org.jetbrains.kotlin.ir.expressions.IrConstructorCall
+import org.jetbrains.kotlin.ir.util.getAnnotation
+import org.jetbrains.kotlin.ir.util.getValueArgument
 import org.jetbrains.kotlin.ir.util.kotlinFqName
 import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.name.Name
 
 private val JS_ASYNC = FqName("seskar.js.JsAsync")
 
+private val OPTIONAL = Name.identifier("optional")
+private fun IrConstructorCall.value(
+    name: Name,
+    defaultValue: Boolean = false,
+): Boolean {
+    val argument = getValueArgument(name) as IrConst<*>?
+        ?: return defaultValue
+
+    return argument.value as Boolean?
+        ?: defaultValue
+}
+
 internal fun IrFunction.getAsyncOptions(): AsyncOptions {
-    require(hasAnnotation(JS_ASYNC)) {
+    val async = getAnnotation(JS_ASYNC)
+
+    requireNotNull(async) {
         "Missed @JsAsync annotation for `${kotlinFqName.asString()}`"
     }
 
-    return AsyncOptions(false)
+    return AsyncOptions(
+        optional = async.value(OPTIONAL),
+    )
 }
