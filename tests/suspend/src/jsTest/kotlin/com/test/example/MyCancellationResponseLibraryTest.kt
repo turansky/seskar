e71e3f40b8e60d9b7d275c1bd94d513d7cfc4aa7
Victor Turansky <victor.turansky@gmail.com>
2024-05-20 02:37:18 +0300

    Suspend. Update tests

--- tests/suspend/src/jsTest/kotlin/com/test/example/MyCancellationResponseLibraryTest.kt
+++ tests/suspend/src/jsTest/kotlin/com/test/example/MyCancellationResponseLibraryTest.kt
@@ -7,6 +7,7 @@ import kotlinx.coroutines.test.runTest
 import web.abort.AbortController
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 import kotlin.time.Duration.Companion.milliseconds
 
 class MyCancellationResponseLibraryTest {
@@ -26,6 +27,25 @@ class MyCancellationResponseLibraryTest {
 
         val rejectExceptions = collector.leave()
 
+        assertTrue(rejectExceptions.isEmpty())
+    }
+
+    private fun runLateCancellationTest(
+        block: suspend CoroutineScope.() -> Unit,
+    ) = runTest {
+        val collector = PromiseRejectionCollector()
+
+        val dataJob = launch(block = block)
+
+        launch {
+            awaitTimeout(300.milliseconds)
+            dataJob.cancel()
+        }
+
+        awaitTimeout(400.milliseconds)
+
+        val rejectExceptions = collector.leave()
+
         assertEquals(1, rejectExceptions.size)
 
         assertEquals(
@@ -41,14 +61,35 @@ class MyCancellationResponseLibraryTest {
         }
 
     @Test
+    fun testGetCancellableResponseOnlyWithOptions_default_lateCancellation() =
+        runLateCancellationTest {
+            getCancellableResponseOnlyWithOptions()
+        }
+
+    @Test
     fun testGetCancellableResponseOnlyWithOptions_emptyOptions() =
         runCancellationTest {
             getCancellableResponseOnlyWithOptions(jso())
         }
 
     @Test
+    fun testGetCancellableResponseOnlyWithOptions_emptyOptions_lateCancellation() =
+        runLateCancellationTest {
+            getCancellableResponseOnlyWithOptions(jso())
+        }
+
+    @Test
     fun testGetCancellableResponseOnlyWithOptions_customSignal() =
         runCancellationTest {
+            val controller = AbortController()
+            getCancellableResponseOnlyWithOptions(jso {
+                signal = controller.signal
+            })
+        }
+
+    @Test
+    fun testGetCancellableResponseOnlyWithOptions_customSignal_lateCancellation() =
+        runLateCancellationTest {
             val controller = AbortController()
             getCancellableResponseOnlyWithOptions(jso {
                 signal = controller.signal
