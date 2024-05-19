639cbc6010e20177646ac0fbbd6847beb5d72ce5
Victor Turansky <victor.turansky@gmail.com>
2024-05-19 15:44:32 +0300

    Suspend. Collect unhandled exceptions

--- tests/suspend/src/jsTest/kotlin/com/test/example/MyCancellationResponseLibraryTest.kt
+++ tests/suspend/src/jsTest/kotlin/com/test/example/MyCancellationResponseLibraryTest.kt
@@ -5,11 +5,15 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
+import kotlin.test.assertContentEquals
 import kotlin.time.Duration.Companion.milliseconds
 
 class MyCancellationResponseLibraryTest {
     @Test
     fun testGetCancellableResponseOnlyWithOptions() = runTest {
+        val handler = UnhandledExceptionHandler()
+        handler.start()
+
         val job = launch {
             getCancellableResponseOnlyWithOptions()
         }
@@ -17,5 +21,10 @@ class MyCancellationResponseLibraryTest {
         delay(100.milliseconds)
         job.cancelAndJoin()
         delay(200.milliseconds)
+
+        assertContentEquals(
+            expected = emptyList(),
+            actual = handler.finish(),
+        )
     }
 }
