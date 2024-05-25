62c17f719bb2f5637c951c45beeeda856a729a3b
Victor Turansky <victor.turansky@gmail.com>
2024-05-25 19:55:14 +0300

    Seskar `2.77.0`

--- seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/KotlinPluginArtifact.kt
+++ seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/KotlinPluginArtifact.kt
@@ -6,5 +6,5 @@ internal val KOTLIN_PLUGIN_ARTIFACT: SubpluginArtifact
     get() = SubpluginArtifact(
         groupId = "io.github.turansky.seskar",
         artifactId = "seskar-compiler-plugin",
-        version = "2.75.0-SNAPSHOT",
+        version = "2.77.1-SNAPSHOT",
     )
