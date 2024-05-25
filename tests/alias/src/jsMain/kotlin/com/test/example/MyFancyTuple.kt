652788901874d540c49f134927b7a065f33028d7
Victor Turansky <victor.turansky@gmail.com>
2024-05-25 17:54:44 +0300

    Alias. Add `MyFancyTuple` test class

---
+++ tests/alias/src/jsMain/kotlin/com/test/example/MyFancyTuple.kt
@@ -0,0 +1,22 @@
+@file:JsModule("my-fancy-tuple")
+
+package com.test.example
+
+import seskar.js.JsAlias
+
+sealed external interface MyFancyTuple<A : Any, B : Any, C : Any> {
+    @JsAlias("[0]")
+    operator fun component1(): A
+
+    @JsAlias("[1]")
+    operator fun component2(): B
+
+    @JsAlias("[2]")
+    operator fun component3(): C
+}
+
+external fun <A : Any, B : Any, C : Any> MyFancyTuple(
+    a: A,
+    b: B,
+    c: C,
+): MyFancyTuple<A, B, C>
---
+++ tests/alias/src/jsMain/resources/my-fancy-tuple.js
@@ -0,0 +1,3 @@
+export function MyFancyTuple(...args) {
+    return args
+}
