e255c48952938a80d4e9edfcb1746ee360f88a54
Victor Turansky <victor.turansky@gmail.com>
2024-08-02 02:14:24 +0300

    KFC `8.7.0`

--- idea-plugin/build.gradle.kts
+++ idea-plugin/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "2.0.0"
     id("org.jetbrains.intellij") version "1.17.3"
-    id("io.github.turansky.kfc.version") version "8.4.0"
+    id("io.github.turansky.kfc.version") version "8.7.0"
 }
 
 repositories {
