4d0ab925d9f0a461fe45271089fde4bdeae6b2df
Victor Turansky <victor.turansky@gmail.com>
2024-08-10 19:33:25 +0300

    Seskar `3.13.0`

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ To add Seskar to your project, you need to add the following configuration to yo
 ```kotlin
 plugins {
     kotlin("multiplatform") version "2.0.10"
-    id("io.github.turansky.seskar") version "3.10.0"
+    id("io.github.turansky.seskar") version "3.13.0"
 }
 ```
 
--- isolated-test/build.gradle.kts
+++ isolated-test/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("multiplatform") apply false
     kotlin("plugin.js-plain-objects") apply false
     id("io.github.turansky.kfc.application")
-    id("io.github.turansky.seskar") version "3.10.0"
+    id("io.github.turansky.seskar") version "3.13.0"
 }
 
 dependencies {
