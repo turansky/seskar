f865d136c6122c6e806cb4cff035a2e533fb9491
Victor Turansky <victor.turansky@gmail.com>
2024-09-11 01:21:31 +0300

    Lazy. Set filename

--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/react/lazy/backend/LazyComponentTransformer.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/react/lazy/backend/LazyComponentTransformer.kt
@@ -3,7 +3,9 @@ package seskar.compiler.react.lazy.backend
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.ir.IrStatement
 import org.jetbrains.kotlin.ir.declarations.IrProperty
+import org.jetbrains.kotlin.ir.util.file
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
+import seskar.compiler.common.backend.JsFileName
 
 internal class LazyComponentTransformer(
     private val context: IrPluginContext,
@@ -11,6 +13,8 @@ internal class LazyComponentTransformer(
     override fun visitProperty(declaration: IrProperty): IrStatement {
         if (!declaration.isLazy())
             return declaration
+
+        declaration.file.annotations += JsFileName(context, "${declaration.name.identifier}.very.very.lazy")
 
         return super.visitProperty(declaration)
     }
