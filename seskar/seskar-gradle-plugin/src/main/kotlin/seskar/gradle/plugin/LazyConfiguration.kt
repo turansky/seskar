2e95e84b244ea0cbb060af794307c0b22e49eb32
Victor Turansky <victor.turansky@gmail.com>
2024-09-12 22:58:15 +0300

    Lazy. Initial plugin implementation

--- seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/LazyConfiguration.kt
+++ seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/LazyConfiguration.kt
@@ -3,16 +3,24 @@ package seskar.gradle.plugin
 class LazyConfiguration(
     val compileTask: String,
     val syncTask: String,
+    val generateTask: String,
 ) {
     companion object {
-        val PRODUCTION = LazyConfiguration(
+        private val PRODUCTION = LazyConfiguration(
             compileTask = "compileProductionExecutableKotlinJs",
             syncTask = "jsProductionExecutableCompileSync",
+            generateTask = "jsProductionGenerateLazyModules",
         )
 
-        val DEVELOPMENT = LazyConfiguration(
+        private val DEVELOPMENT = LazyConfiguration(
             compileTask = "compileDevelopmentExecutableKotlinJs",
             syncTask = "jsDevelopmentExecutableCompileSync",
+            generateTask = "jsDevelopmentGenerateLazyModules",
+        )
+
+        val ALL: List<LazyConfiguration> = listOf(
+            PRODUCTION,
+            DEVELOPMENT,
         )
     }
 }
--- seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/SeskarLazyPlugin.kt
+++ seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/SeskarLazyPlugin.kt
@@ -2,9 +2,30 @@ package seskar.gradle.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Sync
+import org.gradle.kotlin.dsl.create
+
+private const val SESKAR_TASK_GROUP = "seskar"
 
 internal class SeskarLazyPlugin : Plugin<Project> {
-    override fun apply(project: Project) {
-        // TBD
+    override fun apply(project: Project): Unit = with(project) {
+        plugins.withId("io.github.turansky.kfc.application") {
+            for (configuration in LazyConfiguration.ALL) {
+                val generateTask = tasks.create<Sync>(configuration.generateTask) {
+                    group = SESKAR_TASK_GROUP
+
+                    from(tasks.named(configuration.compileTask)) {
+                        include("**/*__lazy__component.mjs")
+                        includeEmptyDirs = false
+                    }
+
+                    into(temporaryDir)
+                }
+
+                tasks.named(configuration.syncTask) {
+                    dependsOn(generateTask)
+                }
+            }
+        }
     }
 }
