62799977e5a2cf601a88dc95dfd7ece3f7babf36
Victor Turansky <victor.turansky@gmail.com>
2024-09-14 03:10:08 +0300

    Lazy. Simple lazy component provider

--- seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/LazyComponentReader.kt
+++ seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/LazyComponentReader.kt
@@ -14,7 +14,57 @@ private fun lazyComponentTransformer(
 ): Reader {
     val writer = StringWriter()
     input.transferTo(writer)
-    val content = writer.toString()
 
-    return StringReader("""import { lazy } from "react"""" + "\n\n" + content)
+    val componentProvider = getComponentProvider(writer.toString())
+        ?: return StringReader("export {}")
+
+    // language=javascript
+    val proxyBody = """
+    import { lazy } from "react"
+
+    const component = lazy(
+        () => Promise.resolve({
+            default: () => "LAZY COMPONENT $componentProvider !!!"
+            /* default: componentProvider.get(), */
+        })
+    )
+    
+    const provider = {
+        get: () => component
+    }
+    
+    export {
+        provider as $componentProvider,
+    }
+    """.trimIndent()
+
+    return StringReader(proxyBody)
+}
+
+private fun getComponentProvider(
+    content: String,
+): String? {
+    val exports = content
+        .substringAfter("\nexport {", "")
+        .substringBefore("\n};", "")
+        .splitToSequence("\n")
+        .map { it.trim() }
+        .map { it.removeSuffix(",") }
+        .filter { it.isNotEmpty() }
+        .map { it.substringAfter(" as ") }
+        .toList()
+
+    if (exports.isEmpty())
+        return null
+
+    val provider = exports.singleOrNull()
+    if (provider != null)
+        return provider
+
+    error(
+        "Unable to create lazy component wrapper from following content:" +
+                "\n-----------------\n" +
+                content +
+                "\n-----------------\n",
+    )
 }
