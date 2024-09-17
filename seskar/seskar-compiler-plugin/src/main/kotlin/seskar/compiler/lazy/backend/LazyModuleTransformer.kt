ff1eb9ad63f3b2bb2ad9cef876fbd411cc9471ef
Victor Turansky <victor.turansky@gmail.com>
2024-09-17 03:32:22 +0300

    Lazy. Check property type

--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/lazy/backend/LazyModuleTransformer.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/lazy/backend/LazyModuleTransformer.kt
@@ -4,10 +4,17 @@ import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.ir.IrStatement
 import org.jetbrains.kotlin.ir.declarations.IrProperty
 import org.jetbrains.kotlin.ir.declarations.name
+import org.jetbrains.kotlin.ir.types.classFqName
 import org.jetbrains.kotlin.ir.util.file
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
+import org.jetbrains.kotlin.name.FqName
 import seskar.compiler.common.backend.JsFileName
 import seskar.compiler.common.backend.JsName
+
+private val TYPE_MAP = mapOf(
+    FqName("js.lazy.LazyFunction") to "lazy__function",
+    FqName("react.FC") to "react__component",
+)
 
 internal class LazyModuleTransformer(
     private val context: IrPluginContext,
@@ -19,10 +26,14 @@ internal class LazyModuleTransformer(
         val fileName = declaration.file.name.removeSuffix(".kt")
         declaration.file.annotations += JsFileName(context, "${fileName}__lazy__module")
 
+        val propertyType = declaration.getter!!.returnType.classFqName
+        val type = TYPE_MAP[propertyType]
+            ?: error("Unsupported lazy property type: $propertyType")
+
         val jsName = sequenceOf(
             fileName,
             declaration.name.identifier,
-            "react__component",
+            type,
         ).joinToString(
             separator = LAZY_DELIMITER,
             prefix = LAZY_DELIMITER,
