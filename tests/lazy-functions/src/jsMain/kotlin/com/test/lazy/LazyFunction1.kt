72d58f624e4490f7bb1a9c11dca2aabfb0a59e7d
Victor Turansky <victor.turansky@gmail.com>
2024-09-18 21:15:23 +0300

    Lazy. Add `@Lazy` annotation

--- tests/lazy-functions/src/jsMain/kotlin/com/test/lazy/LazyFunction1.kt
+++ tests/lazy-functions/src/jsMain/kotlin/com/test/lazy/LazyFunction1.kt
@@ -1,7 +1,9 @@
 package com.test.lazy
 
+import js.lazy.Lazy
 import js.lazy.LazyFunction
 
+@Lazy
 val lazyFunction1 = LazyFunction {
     "42"
 }
--- tests/lazy-functions/src/jsMain/kotlin/com/test/lazy/LazyFunction2.kt
+++ tests/lazy-functions/src/jsMain/kotlin/com/test/lazy/LazyFunction2.kt
@@ -1,7 +1,9 @@
 package com.test.lazy
 
+import js.lazy.Lazy
 import js.lazy.LazyFunction
 
+@Lazy
 val lazyFunction2 = LazyFunction {
     "13"
 }
