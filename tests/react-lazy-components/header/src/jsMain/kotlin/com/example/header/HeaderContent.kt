039ef68440ed8f0848f0c072350c3016882db19d
Victor Turansky <victor.turansky@gmail.com>
2024-09-19 00:58:00 +0300

    Lazy. Cleanup

--- tests/react-lazy-components/header/src/jsMain/kotlin/com/example/header/HeaderContent.kt
+++ tests/react-lazy-components/header/src/jsMain/kotlin/com/example/header/HeaderContent.kt
@@ -1,16 +1,13 @@
 package com.example.header
 
 import react.FC
-import react.Props
 import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.span
 
-internal external interface HeaderContentProps : Props
-
-/** */
-internal val HeaderContent = FC<HeaderContentProps> {
+internal val HeaderContent = FC {
     div {
         +"Header Content"
+
         span {
             +"SUB CONTENT"
         }
