4d14889505a244ddf4376da74dc70a4868cfe781
Victor Turansky <victor.turansky@gmail.com>
2024-09-21 00:53:46 +0300

    Workers. Initial transformation

--- seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/LazyModulePlugin.kt
+++ seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/LazyModulePlugin.kt
@@ -9,6 +9,8 @@ import org.gradle.kotlin.dsl.named
 import org.jetbrains.kotlin.gradle.tasks.IncrementalSyncTask
 import seskar.gradle.plugin.Modules.LAZY_MODULE_SUFFIX
 import seskar.gradle.plugin.Modules.ORIGINAL_MODULE_SUFFIX
+import seskar.gradle.plugin.Workers.GENERATED_WORKER_SUFFIX
+import seskar.gradle.plugin.Workers.WORKER_FACTORY_SUFFIX
 
 internal class LazyModulePlugin : Plugin<Project> {
     override fun apply(project: Project): Unit = with(project) {
@@ -22,6 +24,7 @@ internal class LazyModulePlugin : Plugin<Project> {
                 val generateTask = tasks.create<Sync>(configuration.generateTask) {
                     group = Seskar.TASK_GROUP
 
+                    // lazy
                     from(compileTask) {
                         include("**/*$LAZY_MODULE_SUFFIX")
                         rename { it.removeSuffix(LAZY_MODULE_SUFFIX) + ORIGINAL_MODULE_SUFFIX }
@@ -33,6 +36,25 @@ internal class LazyModulePlugin : Plugin<Project> {
                         include("**/*$LAZY_MODULE_SUFFIX")
 
                         filter(LazyModuleReader::class)
+
+                        includeEmptyDirs = false
+                    }
+
+                    // workers
+                    from(compileTask) {
+                        include("**/*$WORKER_FACTORY_SUFFIX")
+                        rename { it.removeSuffix(WORKER_FACTORY_SUFFIX) + GENERATED_WORKER_SUFFIX }
+
+                        // TODO: add transformation
+
+                        includeEmptyDirs = false
+                    }
+
+                    from(compileTask) {
+                        include("**/*$WORKER_FACTORY_SUFFIX")
+
+                        // TODO: add transformation
+                        // filter(LazyModuleReader::class)
 
                         includeEmptyDirs = false
                     }
--- seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/Workers.kt
+++ seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/Workers.kt
@@ -2,5 +2,5 @@ package seskar.gradle.plugin
 
 object Workers {
     const val WORKER_FACTORY_SUFFIX = "__worker__factory.mjs"
-    const val GENERATED_WORKER = "__generated__worker.mjs"
+    const val GENERATED_WORKER_SUFFIX = "__generated__worker.mjs"
 }
