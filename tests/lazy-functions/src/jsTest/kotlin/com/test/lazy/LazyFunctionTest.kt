6421b5260766b729cea2b7b72c98117ced1a3e41
Victor Turansky <victor.turansky@gmail.com>
2024-09-21 00:21:26 +0300

    Wrappers `pre.810`

--- tests/lazy-functions/src/jsTest/kotlin/com/test/lazy/LazyFunctionTest.kt
+++ tests/lazy-functions/src/jsTest/kotlin/com/test/lazy/LazyFunctionTest.kt
@@ -5,13 +5,14 @@ import kotlin.js.Promise
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertIs
+import js.lazy.invoke
 
 class LazyFunctionTest {
     @Test
     fun lazyFunction1() = runTest {
         assertIs<Promise<*>>(lazyFunction1)
 
-        val data = lazyFunction1.invoke()
+        val data = lazyFunction1()
 
         assertEquals("42", data)
     }
@@ -20,7 +21,7 @@ class LazyFunctionTest {
     fun lazyFunction2() = runTest {
         assertIs<Promise<*>>(lazyFunction2)
 
-        val data = lazyFunction2.invoke()
+        val data = lazyFunction2()
 
         assertEquals("13", data)
     }
