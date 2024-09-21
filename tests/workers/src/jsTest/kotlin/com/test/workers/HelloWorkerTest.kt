93094966ed57c2c7cabfe69a0c958f124af7a913
Victor Turansky <victor.turansky@gmail.com>
2024-09-21 14:07:49 +0300

    Workers. Update simple test

--- tests/workers/src/jsTest/kotlin/com/test/workers/HelloWorkerTest.kt
+++ tests/workers/src/jsTest/kotlin/com/test/workers/HelloWorkerTest.kt
@@ -1,20 +1,21 @@
 package com.test.workers
 
 import kotlinx.coroutines.test.runTest
+import web.workers.Worker
 import kotlin.test.Test
-import kotlin.test.assertTrue
+import kotlin.test.assertIs
 
 class HelloWorkerTest {
     @Test
     fun create() = runTest {
-        /*
         val worker = createHelloWorker()
 
+        assertIs<Worker>(worker)
+
+        /*
         val data = worker.messageEvent.once().data
 
         assertEquals("Hello!", data)
         */
-
-        assertTrue(true)
     }
 }
