891c7e823fea576f812fee5114d715ef751bb376
Victor Turansky <victor.turansky@gmail.com>
2024-09-21 12:40:17 +0300

    Workers. Exports constants

--- seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/Exports.kt
+++ seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/Exports.kt
@@ -1,19 +1,21 @@
 package seskar.gradle.plugin
 
 internal object Exports {
-    const val EMPTY_EXPORTS = "export {}"
+    const val EMPTY_EXPORTS = "export {};"
+    const val EXPORTS_SECTION_START = "\nexport {"
+    const val EXPORTS_SECTION_END = "\n};"
 
     fun getExports(
         content: String,
-    ): List<String> =
-        content
-            .substringAfter("\nexport {", "")
-            .substringBefore("\n};", "")
+    ): List<String> {
+        return content
+            .substringAfter(EXPORTS_SECTION_START, "")
+            .substringBefore(EXPORTS_SECTION_END, "")
             .splitToSequence("\n")
             .map { it.trim() }
             .map { it.removeSuffix(",") }
             .filter { it.isNotEmpty() }
             .map { it.substringAfter(" as ") }
             .toList()
-
+    }
 }
