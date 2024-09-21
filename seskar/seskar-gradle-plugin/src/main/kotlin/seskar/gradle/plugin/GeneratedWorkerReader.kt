9cdfad3e62809a4c3b2356545597f8e083b83265
Victor Turansky <victor.turansky@gmail.com>
2024-09-21 13:35:09 +0300

    Fix formatting

--- seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/GeneratedWorkerReader.kt
+++ seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/GeneratedWorkerReader.kt
@@ -19,7 +19,7 @@ private class GeneratedWorkerTransformer :
     ): String {
         val workerCall = parseExports(exports)
             .singleOrNull()
-            // TODO: create JS error instead
+        // TODO: create JS error instead
             ?: error("Unable to find worker call!")
 
         return "$workerCall()()"
