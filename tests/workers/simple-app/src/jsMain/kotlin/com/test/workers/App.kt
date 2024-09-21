973b70e5aadddbec8cce9565269c2aced5ba14a1
Victor Turansky <victor.turansky@gmail.com>
2024-09-21 23:19:32 +0300

    Workers. Simple app

--- tests/workers/simple-app/src/jsMain/kotlin/com/test/workers/App.kt
+++ tests/workers/simple-app/src/jsMain/kotlin/com/test/workers/App.kt
@@ -1,10 +1,13 @@
 package com.test.workers
 
+import web.console.console
 import web.events.iterator
 import web.workers.messageEvent
 
 suspend fun main() {
     val worker = createHelloWorker()
+
+    console.log("App started!")
 
     for (event in worker.messageEvent) {
         console.log("Message:" + event.data)
