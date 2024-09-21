ec12d9f2fe7b5214582d773aeedb5086c5667d34
Victor Turansky <victor.turansky@gmail.com>
2024-09-21 13:06:34 +0300

    Workers. Add `GeneratedWorkerReader`

---
+++ seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/GeneratedWorkerReader.kt
@@ -0,0 +1,36 @@
+package seskar.gradle.plugin
+
+import seskar.gradle.plugin.Exports.EXPORTS_SECTION_END
+import seskar.gradle.plugin.Exports.EXPORTS_SECTION_START
+import seskar.gradle.plugin.Exports.parseExports
+import java.io.Reader
+
+internal class GeneratedWorkerReader(
+    input: Reader,
+) : FileTransformReader(
+    input = input,
+    transformer = GeneratedWorkerTransformer(),
+)
+
+private class GeneratedWorkerTransformer :
+    FileTransformer {
+    private fun transformExportToCall(
+        exports: String,
+    ): String {
+        val workerCall = parseExports(exports)
+            .singleOrNull()
+            ?: error("Unable to find worker call!")
+
+        return "$workerCall()()"
+    }
+
+    override fun transform(
+        content: String,
+    ): String =
+        replaceBlock(
+            content = content,
+            blockStart = EXPORTS_SECTION_START,
+            blockEnd = EXPORTS_SECTION_END,
+            transform = ::transformExportToCall,
+        )
+}
