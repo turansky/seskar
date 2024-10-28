2900d26e56086874d329dab7c92b4231d2ac92ab
Victor Turansky <victor.turansky@gmail.com>
2024-10-28 23:00:44 +0200

    Events. Use `EventTransformer`

--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/event/extensions/EventTransformExtension.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/event/extensions/EventTransformExtension.kt
@@ -4,13 +4,13 @@ import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.ir.visitors.transformChildrenVoid
-import seskar.compiler.value.backend.ValueTransformer
+import seskar.compiler.event.backend.EventTransformer
 
 internal class EventTransformExtension : IrGenerationExtension {
     override fun generate(
         moduleFragment: IrModuleFragment,
         pluginContext: IrPluginContext,
     ) {
-        moduleFragment.transformChildrenVoid(ValueTransformer(pluginContext))
+        moduleFragment.transformChildrenVoid(EventTransformer(pluginContext))
     }
 }
