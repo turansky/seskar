ff9d1fdcd5ebbfaa5e2053240185290789fbb3db
Victor Turansky <victor.turansky@gmail.com>
2024-10-28 23:19:27 +0200

    Events. Update simple test

--- tests/events/src/jsTest/kotlin/com/test/events/MyTargetTest.kt
+++ tests/events/src/jsTest/kotlin/com/test/events/MyTargetTest.kt
@@ -1,7 +1,8 @@
 package com.test.events
 
+import web.events.EventInstance
 import kotlin.test.Test
-import kotlin.test.assertNotNull
+import kotlin.test.assertIs
 
 class MyTargetTest {
     @Test
@@ -9,6 +10,6 @@ class MyTargetTest {
         val target = MyTarget()
         val dataEvent = target.dataEvent
 
-        assertNotNull(dataEvent)
+        assertIs<EventInstance<*, *, *>>(dataEvent)
     }
 }
