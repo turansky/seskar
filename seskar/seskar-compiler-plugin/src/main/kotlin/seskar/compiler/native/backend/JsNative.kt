95a0713eb03fd48ce1580839a998ebc07e336d43
Victor Turansky <victor.turansky@gmail.com>
2025-02-28 23:28:10 +0200

    Support new "native" annotations

--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/native/backend/JsNative.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/native/backend/JsNative.kt
@@ -1,23 +1,22 @@
 package seskar.compiler.native.backend
 
 import org.jetbrains.kotlin.ir.declarations.IrFunction
-import org.jetbrains.kotlin.ir.util.getAnnotation
+import org.jetbrains.kotlin.ir.util.hasAnnotation
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
 
-private val JS_NATIVE = FqName("seskar.js.JsNative")
-
 private val ANNOTATION_MAP = mapOf(
-    "get" to ClassId(FqName("kotlin.js"), Name.identifier("nativeGetter")),
-    "set" to ClassId(FqName("kotlin.js"), Name.identifier("nativeSetter")),
-
-    "invoke" to ClassId(FqName("kotlin.js"), Name.identifier("nativeInvoke")),
-    "invokeAsync" to ClassId(FqName("kotlin.js"), Name.identifier("nativeInvoke")),
+    FqName("seskar.js.JsNativeGetter") to ClassId(FqName("kotlin.js"), Name.identifier("nativeGetter")),
+    FqName("seskar.js.JsNativeSetter") to ClassId(FqName("kotlin.js"), Name.identifier("nativeSetter")),
+    FqName("seskar.js.JsNativeInvoke") to ClassId(FqName("kotlin.js"), Name.identifier("nativeInvoke")),
 )
 
 internal fun IrFunction.nativeAnnotation(): ClassId? {
-    getAnnotation(JS_NATIVE) ?: return null
+    for ((marker, annotation) in ANNOTATION_MAP) {
+        if (hasAnnotation(marker))
+            return annotation
+    }
 
-    return ANNOTATION_MAP.getValue(name.identifier)
+    return null
 }
