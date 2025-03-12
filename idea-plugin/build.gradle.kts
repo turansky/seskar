abf22efd868f6e2d5af63de9fe1a1f3f3b411834
Victor Turansky <victor.turansky@gmail.com>
2025-03-13 01:34:30 +0200

    KFC `13.9.0`

--- idea-plugin/build.gradle.kts
+++ idea-plugin/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "2.1.10"
     id("org.jetbrains.intellij") version "1.17.3"
-    id("io.github.turansky.kfc.version") version "13.8.0"
+    id("io.github.turansky.kfc.version") version "13.9.0"
 }
 
 repositories {
