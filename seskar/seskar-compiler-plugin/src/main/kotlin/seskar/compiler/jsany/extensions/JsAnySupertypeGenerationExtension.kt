a20a2deaa802ebbad959f0aae1fea84fc6f30a68
Victor Turansky <victor.turansky@gmail.com>
2025-03-13 00:03:26 +0200

    Simple platform check

--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/jsany/extensions/JsAnySupertypeGenerationExtension.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/jsany/extensions/JsAnySupertypeGenerationExtension.kt
@@ -2,6 +2,8 @@ package seskar.compiler.jsany.extensions
 
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.declarations.FirClassLikeDeclaration
+import org.jetbrains.kotlin.fir.declarations.utils.isActual
+import org.jetbrains.kotlin.fir.declarations.utils.isExpect
 import org.jetbrains.kotlin.fir.declarations.utils.isExternal
 import org.jetbrains.kotlin.fir.extensions.FirSupertypeGenerationExtension
 import org.jetbrains.kotlin.fir.types.ConeKotlinType
@@ -11,10 +13,11 @@ import org.jetbrains.kotlin.fir.types.isAny
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
-import org.jetbrains.kotlin.platform.JsPlatform
+
+private val JS_CORE = FqName("js.core")
 
 private val JS_ANY_MARKER = ClassId(
-    FqName("js.core"),
+    JS_CORE,
     Name.identifier("JsAnyMarker"),
 )
 
@@ -26,9 +29,13 @@ internal class JsAnySupertypeGenerationExtension(session: FirSession) :
         if (!declaration.isExternal)
             return false
 
-        val moduleData = declaration.moduleData
-        return moduleData.isCommon
-                && moduleData.platform.any { it is JsPlatform }
+        if (declaration.isExpect)
+            return false
+
+        if (declaration.isActual)
+            return false
+
+        return declaration.moduleData.isCommon
     }
 
     override fun computeAdditionalSupertypes(
