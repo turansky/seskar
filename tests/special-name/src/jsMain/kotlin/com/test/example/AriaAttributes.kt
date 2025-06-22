ce7f1c06bdcf8fd2897b72ea829d064019da7750
Victor Turansky <victor.turansky@gmail.com>
2025-06-22 16:29:54 +0200

    Remove redundant "special name" support

--- tests/special-name/src/jsMain/kotlin/com/test/example/AriaAttributes.kt
+++
@@ -1,8 +0,0 @@
-package com.test.example
-
-import seskar.js.JsSpecialName
-
-external interface AriaAttributes {
-    @JsSpecialName("aria-label")
-    var ariaLabel: String
-}
--- tests/special-name/src/jsMain/kotlin/com/test/example/ElementAttributes.kt
+++
@@ -1,8 +0,0 @@
-package com.test.example
-
-import seskar.js.JsSpecialName
-
-external interface ElementAttributes {
-    @JsSpecialName("get-data")
-    fun getData(): String
-}
