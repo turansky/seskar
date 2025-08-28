126c22240a935e55d0f26f5bf5b0ec2fc85da38a
Victor Turansky <victor.turansky@gmail.com>
2025-08-29 00:51:50 +0200

    Service Worker. Support modules

--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/worklets/backend/WorkletModule.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/worklets/backend/WorkletModule.kt
@@ -10,6 +10,8 @@ internal const val WORKLET_MODULE_DELIMITER = "$$"
 private val WORKLET_MODULES = setOf(
     FqName("web.audio.AudioWorkletModule"),
     FqName("web.css.painting.PaintWorkletModule"),
+
+    FqName("web.serviceworker.ServiceWorkerModule"),
 )
 
 internal fun IrProperty.isWorkletModule(): Boolean =
