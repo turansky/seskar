3bf88c30f0acd993a892421a19498099819a6068
Victor Turansky <victor.turansky@gmail.com>
2025-08-31 21:53:19 +0200

    Service worker. Separate registrar

--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/worklets/backend/WorkletModule.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/worklets/backend/WorkletModule.kt
@@ -10,8 +10,6 @@ internal const val WORKLET_MODULE_DELIMITER = "$$"
 private val WORKLET_MODULES = setOf(
     FqName("web.audio.AudioWorkletModule"),
     FqName("web.css.painting.PaintWorkletModule"),
-
-    FqName("web.serviceworker.ServiceWorkerModule"),
 )
 
 internal fun IrProperty.isWorkletModule(): Boolean =
