d0cdfbfa65888cce2b7348db144e5359ec941c25
Victor Turansky <victor.turansky@gmail.com>
2025-12-17 00:17:56 +0100

    Kotlin `2.3.0`

--- seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/common/extensions/JsCompilerPluginRegistrar.kt
+++ seskar/seskar-compiler-plugin/src/main/kotlin/seskar/compiler/common/extensions/JsCompilerPluginRegistrar.kt
@@ -5,7 +5,6 @@ import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
 abstract class JsCompilerPluginRegistrar : CompilerPluginRegistrar() {
     final override val supportsK2: Boolean = true
 
-    // Compatibility fix for Kotlin `2.3.0`
-    fun getPluginId(): String =
+    final override val pluginId: String =
         "io.github.turansky.seskar"
 }
