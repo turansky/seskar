b42bbc471ce95c417f162b023fe9b7815c77e937
Victor Turansky <victor.turansky@gmail.com>
2026-03-15 01:38:41 +0100

    Cleanup `runReactTest`

--- tests/react-test/src/jsMain/kotlin/testing/library/react/runReactTest.kt
+++ tests/react-test/src/jsMain/kotlin/testing/library/react/runReactTest.kt
@@ -1,7 +1,6 @@
 package testing.library.react
 
 import js.core.Void
-import js.globals.globalThis
 import js.promise.Promise
 import js.test.runJsTest
 import kotlinx.coroutines.CoroutineScope
@@ -13,13 +12,10 @@ import web.html.HTMLElement
 fun runReactTest(
     component: FC<Props>,
     testBody: suspend CoroutineScope.(container: HTMLElement) -> Unit,
-): Promise<Void> {
-    globalThis["IS_REACT_ACT_ENVIRONMENT"] = true
-
-    return runJsTest {
+): Promise<Void> =
+    runJsTest {
         val result = render(component.create())
         val container = result.container as HTMLElement
 
         testBody(container)
     }
-}
