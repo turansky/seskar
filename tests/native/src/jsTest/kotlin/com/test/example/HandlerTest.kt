88ca77d7db31c0d9db025c2cd2fd86ac2543418a
Victor Turansky <victor.turansky@gmail.com>
2026-03-18 01:55:53 +0100

    Update "native invoke" test

--- tests/native/src/jsTest/kotlin/com/test/example/HandlerTest.kt
+++ tests/native/src/jsTest/kotlin/com/test/example/HandlerTest.kt
@@ -1,12 +1,15 @@
 package com.test.example
 
+import js.reflect.unsafeCast
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class HandlerTest {
     @Test
     fun test() {
-        val handler: MyHandler = ({ x: Int, y: Int -> x + y }).unsafeCast<MyHandler>()
+        val handler: MyHandler = unsafeCast { x: Int, y: Int ->
+            x + y
+        }
 
         assertEquals(55, handler(13, 42))
     }
