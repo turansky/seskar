3edacc98f1a6845cb75c7400d0633ebbbfe50176
porotkin <preload737773@gmail.com>
2026-05-22 15:55:11 +0300

    IncrementalSyncTask. Transform directories only

--- seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/IncrementalSyncTask.ext.kt
+++ seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/IncrementalSyncTask.ext.kt
@@ -1,9 +1,11 @@
 package seskar.gradle.plugin
 
-import org.gradle.api.Task
 import org.gradle.api.file.Directory
-import org.gradle.api.provider.Provider
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.internal.provider.ProviderInternal
 import org.jetbrains.kotlin.gradle.tasks.IncrementalSyncTask
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
 
 private val ORIGINAL_FILE_FILTERS: List<String> =
     GenerationOptions.entries
@@ -13,18 +15,26 @@ internal fun IncrementalSyncTask.excludeOriginalSources() {
     from.setFrom(from.from.map(::withDirectoryFilter))
 }
 
-private fun Task.withDirectoryFilter(
+private fun withDirectoryFilter(
     source: Any,
 ): Any {
-    if (source !is Provider<*>)
+    if (!isDirectoryProvider(source))
         return source
 
     return source.map { value ->
-        if (value !is Directory)
-            return@map value
-
-        project.fileTree(value) {
+        value.asFileTree.matching {
             exclude(ORIGINAL_FILE_FILTERS)
         }
     }
+}
+
+@OptIn(ExperimentalContracts::class)
+private fun isDirectoryProvider(
+    source: Any?,
+): Boolean {
+    contract {
+        returns(true) implies (source is DirectoryProperty)
+    }
+
+    return source is ProviderInternal<*> && source.type == Directory::class.java
 }
