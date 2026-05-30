49378b1456f15dd647b2356dc84b050ceda7c473
Victor Turansky <victor.turansky@gmail.com>
2026-05-30 04:15:53 +0200

    Strict "is kotlin compile directory provider"

--- seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/IncrementalSyncTask.ext.kt
+++ seskar/seskar-gradle-plugin/src/main/kotlin/seskar/gradle/plugin/IncrementalSyncTask.ext.kt
@@ -1,7 +1,10 @@
 package seskar.gradle.plugin
 
 import org.gradle.api.file.Directory
+import org.gradle.api.internal.provider.ValueSupplier
+import org.gradle.api.internal.provider.ValueSupplier.TaskProducer
 import org.gradle.api.provider.Provider
+import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
 import org.jetbrains.kotlin.gradle.tasks.IncrementalSyncTask
 
 private val ORIGINAL_FILE_FILTERS: List<String> =
@@ -15,15 +18,41 @@ internal fun IncrementalSyncTask.excludeOriginalSources() {
 private fun withDirectoryFilter(
     source: Any,
 ): Any {
-    if (source !is Provider<*>)
+    if (!isKotlinCompileDirectoryProvider(source))
         return source
 
-    return source.map { value ->
-        if (value !is Directory)
-            return@map value
-
-        value.asFileTree.matching {
-            exclude(ORIGINAL_FILE_FILTERS)
+    return (source as Provider<*>)
+        .map { it as Directory }
+        .map { directory ->
+            directory.asFileTree.matching {
+                exclude(ORIGINAL_FILE_FILTERS)
+            }
         }
+}
+
+private fun isKotlinCompileDirectoryProvider(
+    source: Any,
+): Boolean {
+    if (source !is Provider<*>)
+        return false
+
+    if (source !is ValueSupplier)
+        return false
+
+    val producer = source.producer as? TaskProducer
+        ?: return false
+
+    val values = mutableListOf<Boolean>()
+    producer.visitContentProducerTasks {
+        values.add(this is KotlinJsCompile)
     }
+
+    if (values.none { it })
+        return false
+
+    require(values.size == 1) {
+        "Expected exactly one 'KotlinJsCompile' task"
+    }
+
+    return true
 }
